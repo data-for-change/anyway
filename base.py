5cbc2d16d11af44667d6f19a185b097d9f46ca9c
Omri Baumer <omri@sbsh.net>
2013-10-14 21:53:55 +0300

    Work on wrappers

--- base.py
+++ base.py
@@ -1,30 +1,33 @@
 from functools import wraps
+from flask import session, redirect
+from flask import request
 from models import *
 from functools import wraps
 
 def get_user():
-    pass
+	if "user" in session:
+		return session["user"]
+	else:
+		return None
 
 def user_optional(handler):
 
 	@wraps(handler)
-	def check_login(self, *args, **kwargs):
-		self.user = get_user()
-		return handler(self, *args, **kwargs)
+	def check_login(*args, **kwargs):
+		return handler(*args, **kwargs)
 
 	return check_login
 
 def user_required(handler):
 
 	@wraps(handler)
-	def check_login(self, *args, **kwargs):
+	def check_login(*args, **kwargs):
 		user = get_user()
 		if not user:
-			self.session["last_page_before_login"] = self.request.path + "?" + self.request.query_string
-			self.redirect("/")
+			session["last_page_before_login"] = request.path + "?" + request.query_string
+			return redirect("/")
 		else:
-			self.user = user
-			return handler(self, *args, **kwargs)
+			return handler(*args, **kwargs)
 
     	return check_login
 

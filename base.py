3a926a51c1337c78168c5dad9eecd6be3e752ccf
Omri Baumer <omri@sbsh.net>
2013-10-14 20:58:14 +0300

    Add Wraps to base.py decorators

--- base.py
+++ base.py
@@ -1,26 +1,32 @@
+from functools import wraps
 from models import *
+from functools import wraps
 
 def get_user():
     pass
 
 def user_optional(handler):
-    def check_login(self, *args, **kwargs):
-        self.user = get_user()
-        return handler(self, *args, **kwargs)
 
-    return check_login
+	@wraps(handler)
+	def check_login(self, *args, **kwargs):
+		self.user = get_user()
+		return handler(self, *args, **kwargs)
+
+	return check_login
 
 def user_required(handler):
-    def check_login(self, *args, **kwargs):
-        user = get_user()
-        if not user:
-            self.session["last_page_before_login"] = self.request.path + "?" + self.request.query_string
-            self.redirect("/")
-        else:
-            self.user = user
-            return handler(self, *args, **kwargs)
 
-    return check_login
+	@wraps(handler)
+	def check_login(self, *args, **kwargs):
+		user = get_user()
+		if not user:
+			self.session["last_page_before_login"] = self.request.path + "?" + self.request.query_string
+			self.redirect("/")
+		else:
+			self.user = user
+			return handler(self, *args, **kwargs)
+
+    	return check_login
 
 
 

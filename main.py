dec217fbd9438fca65bda9d31f336a359663b278
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2014-07-21 20:03:32 +0300

    Avoid unclear exception when trying to download empty CSV

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import json
 import urllib
 import jinja2
 import csv
-import StringIO
+from StringIO import StringIO
 
 from flask import Flask, request, make_response
 from flask.ext.sqlalchemy import SQLAlchemy
@@ -56,20 +56,22 @@ def markers(methods=["GET", "POST"]):
             markers = [marker.serialize() for marker in results.all()]
 
         if request.values.get('format') == 'csv':
-            output_file = StringIO.StringIO()
-            output = csv.DictWriter(output_file, markers[0].keys())
+            if not markers:
+                raise Exception("No markers available.")
 
+            output_file = StringIO()
+            output = csv.DictWriter(output_file, markers[0].keys())
             output.writeheader()
             for marker in markers:
                 row = {k: v.encode('utf8')
                        if type(v) is unicode else v
                        for k, v in marker.iteritems()}
-
                 output.writerow(row)
 
             return make_response((output_file.getvalue(),
                 'test',
                 {u'Content-Disposition', u'attachment; filename="data.csv"'}))
+
         else: # defaults to json
             return make_response(json.dumps(markers))
 

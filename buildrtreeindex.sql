c0e8967ebb83f0813d3730967029148c7dcc7859
Elad Karakuli <elad.karakuli@gmail.com>
2015-04-30 16:17:26 +0300

    clean branch, remove unnecessary file

--- buildrtreeindex.sql
+++
@@ -1,5 +0,0 @@
-CREATE VIRTUAL TABLE marker_index USING rtree(
-   id,        -- Integer primary key
-   longitude,      -- Minimum and maximum longitude
-   latitude       -- Minimum and maximum latitude
-);

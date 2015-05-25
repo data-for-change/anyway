c29c2bb6e6c82e8697bbb41675510e987bbf8095
Avi Klaiman <aviklaiman@hotmail.com>
2015-05-25 20:33:32 +0300

    #251 - Adjusted sending emails through Sendgrid.

--- main.py
+++ main.py
@@ -23,14 +23,14 @@ import flask_login as login
 from flask_admin.contrib import sqla
 from flask_admin import helpers, expose, BaseView
 from werkzeug.security import check_password_hash
-from sendgrid import sendgrid, SendGridClientError, SendGridServerError
+from sendgrid import sendgrid, SendGridClientError, SendGridServerError, Mail
 
 app = utilities.init_flask(__name__)
 app.config.from_object(__name__)
 
 assets = flask.ext.assets.Environment()
 assets.init_app(app)
-sg = sendgrid.SendGridClient('anywaytest', 'anyway987', raise_errors=True)
+sg = sendgrid.SendGridClient(app.config['SENDGRID_USERNAME'], app.config['SENDGRID_PASSWORD'], raise_errors=True)
 
 assets_env = AssetsEnvironment('./static/', '/static')
 jinja_environment = jinja2.Environment(
@@ -362,10 +362,10 @@ class SendToSubscribersView(BaseView):
         else:
             jsondata = request.get_json(force=True)
             users_send_email_to = db_session.query(User).filter(User.new_features_subscription == True)
-            message = sendgrid.Mail()
+            message = Mail()
             message.set_subject(jsondata['subject'].encode("utf8"))
             message.set_text(jsondata['message'].encode("utf8"))
-            message.set_from('ANYWAY Team <anywaytest@sendgrid.com>')
+            message.set_from('ANYWAY Team <feedback@anyway.co.il>')
             for user in users_send_email_to:
                 message.add_bcc(user.email)
             try:
@@ -374,7 +374,7 @@ class SendToSubscribersView(BaseView):
                 return "Error occurred while trying to send the emails"
             except SendGridServerError:
                 return "Error occurred while trying to send the emails"
-            return "O.K"
+            return "Email/s Sent"
 
     def is_visible(self):
         return login.current_user.is_authenticated()

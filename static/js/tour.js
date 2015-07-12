a88b4f6461e181ca283fa3fe267abcbbe9b21829
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2015-07-12 10:59:26 +0300

    Indentation fixes

--- static/js/tour.js
+++ static/js/tour.js
@@ -17,68 +17,68 @@ var tour1 = new Tour({
         </div> \
     </nav> \
     </div>",
-  steps: [
-  {
-    element:  '#tour-control',
-    animation: true,
-    title: "ברוכים הבאים!",
-    placement: 'bottom',
-    content: '<p>ברוכים הבאים ל-ANYWAY. על גבי המפה ניתן לראות היכן התרחשו תאונות דרכים עם נפגעים.</p>',
-      onShown: function(){
-          $('#step-0').find('[data-role="prev"]').prop('disabled', true);
-      },
-      onNext: function(){
-        tourAddInput();
-    }
-  },
-  {
-    element:  '#pac-input',
-    title: "חיפוש כתובת",
-    animation: true,
-    placement: 'bottom',
-    content: '<p>כאן תוכלו להזין את כתובתכם או כתובת שאתם נוהגים לבקר בה.</p></br>'+'<p> לחיצה על ENTER תיקח אתכם אל הכתובת. לדוגמא:</br>ז\'בוטנסקי 74 פתח תקווה</p>',
-    onNext: function () {
-        stepId = 'step-2'
-        tourClickInput();
-    }
-  },
-{
-    element:  '#step3tour',
-    title: "סינון לפי חומרה",
-    placement: 'left',
-    content: '<p>כאן ניתן להציג או להסתיר תאונות ברמות חומרה שונות. </br>לצורך הדוגמא נבטל את הצגתן של תאונות קלות על ידי ביטול הסימון.</br> במידה ותבחרו להציג תאונות שמיקומן מוערך שימו לב שמידת ההערכה תופיע בשדה "עיגון" בפרטי התאונה ואייקון התאונה יהיה שקוף. </p>',
-    onPrev: function(){
-        $('*[data-type="3"]').click();
-        step3prev();
-    },
-     onNext: function(){
-        tourLocation = 3;
-     }
-},
-{
-    element:  '#step4tour',
-    title: "סינון לפי טווח תאריכים ",
-    placement: 'left',
-    content: '<p>כאן ניתן לבחור טווח תאריכים להצגת תאונות.</br>לנוחותכם ישנם קיצורי דרך לשנה ספציפית וכן אפשרות לבחור כל טווח תאריכים באופן ידני.</br> לצורך הדוגמא נבחר להציג תאונות מינואר 2006 עד ינואר 2014.</p>',
-    onNext: function(){
-        tour4next();
-    }
-},
-{
-    element:  '#step6tour',
-    title: "רשימת התאונות המוצגות על המפה",
-    placement: 'left',
-    content: '<p>כאן תתעדכן רשימת המצגות על גבי המפה לפי תאריך בסדר יורד, מעבר על פריט ברשימה תקפיץ אותו על המפה, ולחיצה תפתח את פרטיו על גבי המפה.</p>',
-    onPrev: function(){
-        tour6prev();
-    }
-},{
-    element:  '#js-embed-link',
-    title: "קישור לתצוגה הנוכחית",
-    placement: 'bottom',
-    content: '<p>כאן תתעדכן רשימת המצגות על גבי המפה לפי תאריך בסדר יורד, מעבר על פריט ברשימה תקפיץ אותו על המפה, ולחיצה תפתח את פרטיו על גבי המפה.</p>'
-},
-  ]});
+    steps: [
+        {
+            element:  '#tour-control',
+            animation: true,
+            title: "ברוכים הבאים!",
+            placement: 'bottom',
+            content: '<p>ברוכים הבאים ל-ANYWAY. על גבי המפה ניתן לראות היכן התרחשו תאונות דרכים עם נפגעים.</p>',
+            onShown: function(){
+                $('#step-0').find('[data-role="prev"]').prop('disabled', true);
+            },
+            onNext: function(){
+                tourAddInput();
+            }
+        },
+        {
+            element:  '#pac-input',
+            title: "חיפוש כתובת",
+            animation: true,
+            placement: 'bottom',
+            content: '<p>כאן תוכלו להזין את כתובתכם או כתובת שאתם נוהגים לבקר בה.</p></br>'+'<p> לחיצה על ENTER תיקח אתכם אל הכתובת. לדוגמא:</br>ז\'בוטנסקי 74 פתח תקווה</p>',
+            onNext: function () {
+                stepId = 'step-2'
+                tourClickInput();
+            }
+        },
+        {
+            element:  '#step3tour',
+            title: "סינון לפי חומרה",
+            placement: 'left',
+            content: '<p>כאן ניתן להציג או להסתיר תאונות ברמות חומרה שונות. </br>לצורך הדוגמא נבטל את הצגתן של תאונות קלות על ידי ביטול הסימון.</br> במידה ותבחרו להציג תאונות שמיקומן מוערך שימו לב שמידת ההערכה תופיע בשדה "עיגון" בפרטי התאונה ואייקון התאונה יהיה שקוף. </p>',
+            onPrev: function(){
+                $('*[data-type="3"]').click();
+                step3prev();
+            },
+            onNext: function(){
+                tourLocation = 3;
+            }
+        },
+        {
+            element:  '#step4tour',
+            title: "סינון לפי טווח תאריכים ",
+            placement: 'left',
+            content: '<p>כאן ניתן לבחור טווח תאריכים להצגת תאונות.</br>לנוחותכם ישנם קיצורי דרך לשנה ספציפית וכן אפשרות לבחור כל טווח תאריכים באופן ידני.</br> לצורך הדוגמא נבחר להציג תאונות מינואר 2006 עד ינואר 2014.</p>',
+            onNext: function(){
+                tour4next();
+            }
+        },
+        {
+            element:  '#step6tour',
+            title: "רשימת התאונות המוצגות על המפה",
+            placement: 'left',
+            content: '<p>כאן תתעדכן רשימת המצגות על גבי המפה לפי תאריך בסדר יורד, מעבר על פריט ברשימה תקפיץ אותו על המפה, ולחיצה תפתח את פרטיו על גבי המפה.</p>',
+            onPrev: function(){
+                tour6prev();
+            }
+        },{
+            element:  '#js-embed-link',
+            title: "קישור לתצוגה הנוכחית",
+            placement: 'bottom',
+            content: '<p>כאן תתעדכן רשימת המצגות על גבי המפה לפי תאריך בסדר יורד, מעבר על פריט ברשימה תקפיץ אותו על המפה, ולחיצה תפתח את פרטיו על גבי המפה.</p>'
+        },
+    ]});
 // Initialize the tour
 tour1.init();
 resetTour();// Start the tour
@@ -113,7 +113,7 @@ function step2next() {
     if (tourLocation != 5 && tourLocation != 6){
         if (checkStepsAdd) {
             for (i = 0; i < 4; i++) {
-    tour.addStep(tour1.getStep(i));
+                tour.addStep(tour1.getStep(i));
             }
             tour.init();
             tour.restart();
@@ -122,14 +122,14 @@ function step2next() {
             tour.addStep(tour1.getStep(2));
             tour.addStep(tour1.getStep(3));
             checkStepsAdd = true;
-            }
+        }
         tour.goTo(2);
         $('*[data-type="3"]').click();
     }
-    else if (tourLocation == 5 || tourLocation == 6){
+    else{
         if (checkStepsAdd2) {
             for (i = 0; i < 6; i++) {
-    tour.addStep(tour1.getStep(i));
+                tour.addStep(tour1.getStep(i));
             }
             tour.init();
             tour.restart()
@@ -213,25 +213,25 @@ function defInfoWindows(){
 // style for tourInfoWindow
 function tourStyle(infowindow){
     google.maps.event.addListener(infowindow, 'domready', function() {
-    $('#tourInfoWindow').parent().parent().parent().parent().attr('class', 'gm-style-iwtour');
-    iwOuter = $('.gm-style-iwtour');
-    console.log(iwOuter.attr('class'));
+        $('#tourInfoWindow').parent().parent().parent().parent().attr('class', 'gm-style-iwtour');
+        iwOuter = $('.gm-style-iwtour');
+        console.log(iwOuter.attr('class'));
         iwBackground = iwOuter.prev();
-    iwBackground.children(':nth-child(2)').css({'display' : 'none'});
-    iwBackground.children(':nth-child(4)').css({'display' : 'none'});
-    iwBackground.children(':nth-child(3)').find('div').children().css({'box-shadow': '0.5px 0.5px 0.5px 1px #bbbbbb;', 'z-index' : '1'});
-    iwCloseBtn = iwOuter.next();
-    iwCloseBtn.css({
-        opacity: '0.3', // by default the close button has an opacity of 0.7
-        left: '55px', top: '20px', // button repositioning
+        iwBackground.children(':nth-child(2)').css({'display' : 'none'});
+        iwBackground.children(':nth-child(4)').css({'display' : 'none'});
+        iwBackground.children(':nth-child(3)').find('div').children().css({'box-shadow': '0.5px 0.5px 0.5px 1px #bbbbbb;', 'z-index' : '1'});
+        iwCloseBtn = iwOuter.next();
+        iwCloseBtn.css({
+            opacity: '0.3', // by default the close button has an opacity of 0.7
+            left: '55px', top: '20px', // button repositioning
+        });
+        iwCloseBtn.mouseover(function(){
+            $(this).css({opacity: '0.5'});
+        });
+        iwCloseBtn.mouseout(function(){
+            $(this).css({opacity: '0.3'});
+        });
     });
-    iwCloseBtn.mouseover(function(){
-        $(this).css({opacity: '0.5'});
-    });
-    iwCloseBtn.mouseout(function(){
-      $(this).css({opacity: '0.3'});
-    });
-});
 }
 function restInfoWindow(iwOuter,iwBackground,iwCloseBtn){
     iwOuter.attr("class","gm-style-iw");
@@ -245,7 +245,7 @@ function restInfoWindow(iwOuter,iwBackground,iwCloseBtn){
         $(this).css({opacity: '0.8'});
     });
     iwCloseBtn.mouseout(function(){
-      $(this).css({opacity: '0.7'});
+        $(this).css({opacity: '0.7'});
     });
 }
 

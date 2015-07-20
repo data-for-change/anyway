f9bfa558ce8f2b81022a08a0bf0e3dbedc856617
Omer <omerxx@gmail.com>
2015-07-20 20:34:41 +0300

    Changed default year to 2013, added <= in bounding box filters, changed to assertTrue/assertFalse tests

--- tests/test_bounding_box_query.py
+++ tests/test_bounding_box_query.py
@@ -2,13 +2,17 @@ import unittest
 from models import Marker  # for Marker.bounding_box_query
 import datetime
 
+# This tests year 2013 accidents as this is the current example git data for testing
+# Once this changes to another year or to the current year's accidents (as should be) un-comment lines 11,13,15
+# and change both 2013 and 2014 to: %s
+
 
 class TestQueryFilters(unittest.TestCase):
-    cyear = str(datetime.datetime.now().strftime("%Y"))
+    # cyear = str(datetime.datetime.now().strftime("%Y"))
     global start_date
-    start_date = "01/01/%s" % cyear
+    start_date = "01/01/2013"     # % cyear
     global end_date
-    end_date = "01/01/%s" % str(int(cyear)-1)
+    end_date = "01/01/2014"       # % str(int(cyear)-1)
 
     def setUp(self):
         self.query = Marker.bounding_box_query(ne_lat=32.36, ne_lng=35.088, sw_lat=32.292, sw_lng=34.884,
@@ -21,18 +25,18 @@ class TestQueryFilters(unittest.TestCase):
 
     def test_location_filters(self):
         for marker in self.query:
-            self.assertEqual(self.query['sw_lat'] < marker.latitude < self.query['ne_lat'], True)
-            self.assertEqual(self.query['sw_lng'] < marker.longitude < self.query['ne_lng'], True)
+            self.assertTrue(self.query['sw_lat'] <= marker.latitude <= self.query['ne_lat'])
+            self.assertTrue(self.query['sw_lng'] <= marker.longitude <= self.query['ne_lng'])
 
     def test_accuracy_filter(self):
         for marker in self.query:
-            self.assertEqual(marker['inaccurate'], False)
+            self.assertFalse(marker['inaccurate'])
 
     def test_severity_filters(self):
         for marker in self.query:
-            self.assertEqual(marker['fatal'], False)
-            self.assertEqual(marker['severe'], True)
-            self.assertEqual(marker['light'], True)
+            self.assertFalse(marker['fatal'])
+            self.assertTrue(marker['severe'])
+            self.assertTrue(marker['light'])
 
 
 if __name__ == '__main__':

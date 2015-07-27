c89aa7f34aa3f3efaf67c827ce346e5dc9579215
Omer <omerxx@gmail.com>
2015-07-27 18:52:47 +0300

    added accident id and provider code, added date to its section

--- static/js/marker.js
+++ static/js/marker.js
@@ -54,6 +54,8 @@ var MarkerView = Backbone.View.extend({
 
         this.$el.width(400);
         this.$el.find(".title").text(this.marker.get("title"));
+        this.$el.find(".id").text(fields.ACC_ID + ": " + this.marker.get("id"));
+        this.$el.find(".provider_code").text(fields.PROVIDER_CODE + ": " + this.model.get("provider_code"));
         this.$el.find(".roadType").text(fields.SUG_DEREH + ": " + localization.SUG_DEREH[this.model.get("roadType")]);
         this.$el.find(".accidentType").text(fields.SUG_TEUNA+ ": " + localization.SUG_TEUNA[this.model.get("subtype")]);
         this.$el.find(".roadShape").text(fields.ZURAT_DEREH+ ": " + localization.ZURAT_DEREH[this.model.get("roadShape")]);

6ad8535ec27c02c9500c6345b710e94ae8ca30f1
Omer <omerxx@gmail.com>
2015-09-10 11:53:07 +0300

    Added united.py for loading united hatzala data to DB

--- models.py
+++ models.py
@@ -190,7 +190,9 @@ class Marker(MarkerMixin, Base): # TODO rename to AccidentMarker
         if not is_thin:
             fields.update({
                 "title": self.title,
-                "description": Marker.json_to_description(self.description),
+                # CHANGE FOR UNITED:
+                # "description": Marker.json_to_description(self.description),
+                "description": self.description,
                 "address": self.address,
                 "type": self.type,
                 "subtype": self.subtype,

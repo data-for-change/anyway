0e9a14cc91f89478a0d6060c6ddeabb046c55e3c
Omer <omerxx@gmail.com>
2015-09-13 00:51:55 +0300

    Applied incremental data import and dealt with unique IDs

--- united.py
+++ united.py
@@ -3,10 +3,10 @@ from datetime import datetime
 from flask.ext.sqlalchemy import SQLAlchemy
 from models import Marker
 from utilities import init_flask
-from process import time_delta
 
 app = init_flask(__name__)
 db = SQLAlchemy(app)
+location = "static/data/united/united_data.csv"
 
 def parse_date(created):
     time = datetime.strptime(created, '%m/%d/%y %H:%M')
@@ -18,74 +18,40 @@ def parse_date(created):
     return datetime(year, month, day, hour, minute, 0)
 
 def import_accidents():
-    # print("\tReading accidents data from '%s'..." % os.path.basename(accidents))
-    print("\tReading accidents data from united files")
+    print("\tReading accidents data from '%s'..." % location)
     header = True
-    with open("static/data/united/united_data.csv", 'rU') as f:
+    inc = 1
+    with open(location, 'rU') as f:
         reader = csv.reader(f, delimiter=',', dialect=csv.excel_tab)
         for accident in reader:
             if header:
                 header = False
                 continue
 
-            """
-            if "Latitude" not in accident or "Longtitude" not in accident:
-                raise ValueError("Missing x and y coordinates")
-            """
             marker = {
                 "latitude": accident[1],
                 "longitude": accident[2],
                 "created": parse_date(accident[0]),
-                # "provider_code": int(''.join(x for x in accident[0] if x.isdigit()))*2,
-                "provider_code": 3,
-                "id": int(''.join(x for x in accident[0] if x.isdigit())),
+                "provider_code": 0,
+                "id": int(''.join(x for x in accident[0] if x.isdigit()) + str(inc)),
                 "title": "Accident",
-                "description": "",
                 "address": accident[3] + ' ' + accident[5],
-                "subtype": 15,
-                "roadType": 5,
-                "roadShape": 9,
-                "dayType": 4,
-                "unit": 0,
-                "mainStreet": "",
-                "secondaryStreet": "",
-                "junction": "",
-                "one_lane": 9,
-                "multi_lane": -1,
-                "speed_limit": -1,
-                "intactness": -1,
-                "road_width": -1,
-                "road_sign": 5,
-                "road_light": 1,
-                "road_control": -1,
-                "weather": 9,
-                "road_surface": 9,
-                "road_object": 9,
-                "object_distance": 9,
-                "didnt_cross": 9,
-                "cross_mode": -1,
-                "cross_location": -1,
-                "cross_direction": 9,
-                "severity": 4,
-                "locationAccuracy": 1
-
+                "severity": 3,
+                "locationAccuracy": 1,
+                "subtype": 21           # New subtype for United Hatzala
             }
 
+            inc += 1
             yield marker
 
 
 def import_to_datastore():
-    """
-    goes through all the files in a given directory, parses and commits them
-    """
     try:
-        started = datetime.now()
-
         accidents = list(import_accidents())
-        db.session.execute(Marker.__table__.insert(), accidents)
+        db.session.execute(Marker.__table__.insert().prefix_with("OR IGNORE"), accidents)
         db.session.commit()
 
-        print("\t{0} items in {1}".format(len(accidents), time_delta(started)))
+        print("\tImported {0} items".format(len(accidents)))
         return len(accidents)
     except ValueError as e:
         print e.message
@@ -93,10 +59,7 @@ def import_to_datastore():
 
 
 def main():
-    # TODO: implement only import of incremental data
-    # TODO 2: Handle unique id's!!!
     import_to_datastore()
-    # print parse_date("8/31/15 12:08")
 
 if __name__ == "__main__":
     main()

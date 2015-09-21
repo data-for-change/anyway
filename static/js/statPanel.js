b5269d188fc32fb27c1f4c2e4ba33b3273501fed
Avi Klaiman <aviklaiman@hotmail.com>
2015-09-21 23:45:31 +0300

    #417 - changed charts framework to FusionCharts - will be useful later for more complicated charts. It's free for open source projects.

--- static/js/statPanel.js
+++ static/js/statPanel.js
@@ -7,7 +7,7 @@ function statPanelClick(widthOfPanel, heightOfPanel, chartWidth, chartHeight) {
  	lang: 'he'
  	},
  	size: { width: widthOfPanel, height: heightOfPanel },
- 	title: 'מספר התאונות בחודש',
+ 	title: 'קצת גרפים',
  	position: 'center',
     id: 'statPanel',
 	theme: 'light',
@@ -20,57 +20,43 @@ function statPanelClick(widthOfPanel, heightOfPanel, chartWidth, chartHeight) {
 };
 
 var startJSPanelWithChart = function( jsPanel, widthOfPanel, heightOfPanel, chartWidth, chartHeight ){
-	var str = '<canvas id="myChart" width="';
+	var str = '<div id="myChart" width="';
 	str+= chartWidth.toString();
 	str+= '" height="';
 	str+= chartHeight.toString();
-	str+= '"></canvas>';
+	str+= '"></div>';
 	jsPanel.content.empty();
 	jsPanel.content.append(str);
 	//jsPanel.content.append('<canvas id="myChart" width=chartWidth height=chartHeight></canvas>')
-	var ctx = $("#myChart").get(0).getContext("2d");
-	var myNewChart = new Chart(ctx);
 
-	var groupedByMonth = _.groupBy(app.markers.pluck("created"), function(item) {
+
+    var groupedByMonth = _.countBy(app.markers.pluck("created"), function(item) {
 		return item.substring(0,7);
 	});
 
-	var monthSorted = Object.keys(groupedByMonth).sort();
-	var numberOfMonths = Object.keys(groupedByMonth).length;
-	var numberOfAccidentsPerMonth = [];
-	for (i = 0; i <= numberOfMonths-1; i++) {
-		numberOfAccidentsPerMonth.push(groupedByMonth[Object.keys(groupedByMonth)[i]].length);
-	}
+    var jsonMonth = _.map(groupedByMonth, function(group, key) {return {"label": key, "value": group.toString()}});
+	jsonMonth = _.sortBy(jsonMonth, 'label');
 
-	var data = {
-		labels: monthSorted,
-		datasets: [
-			{
-				label: "Number of accidents",
-				labelColor : 'black',
-                labelFontSize : '16',
-				fillColor: "rgba(220,220,220,0.2)",
-				strokeColor: "rgba(220,220,220,1)",
-				pointColor: "rgba(220,220,220,1)",
-				pointStrokeColor: "#fff",
-				pointHighlightFill: "#fff",
-				pointHighlightStroke: "rgba(220,220,220,1)",
-				data: numberOfAccidentsPerMonth
-			}//,
-			//{
-			//	label: "My Second dataset",
-			//	fillColor: "rgba(151,187,205,0.2)",
-			//	strokeColor: "rgba(151,187,205,1)",
-			//	pointColor: "rgba(151,187,205,1)",
-			//	pointStrokeColor: "#fff",
-			//	pointHighlightFill: "#fff",
-			//	pointHighlightStroke: "rgba(151,187,205,1)",
-			//	data: app.markers.pluck("weather")
-			//}
-		]
-	};
+	FusionCharts.ready(function(){
+		  var revenueChart = new FusionCharts({
+			"type": "column2d",
+			"renderAt": "myChart",
+			"width": chartWidth,
+			"height": chartHeight,
+			"dataFormat": "json",
+			"dataSource": {
+			  "chart": {
+				  "caption": "מספר התאונות בחודש",
+				  "xAxisName": "חודשים",
+				  "yAxisName": "מספר תאונות",
+				  "theme": "fint"
+			   },
+			  "data": jsonMonth
+			 }
+			});
 
-	var myLineChart = new Chart(ctx).Line(data);
+			revenueChart.render();
+		});
 	};
 
 $('body').on( "jspanelloaded", function( event, id ){

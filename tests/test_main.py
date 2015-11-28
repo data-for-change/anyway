a8a2f5b77a72a3d115a610f390ab3ecb17aec629
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2015-11-28 13:12:03 +0200

    Fix unit test

--- tests/test_main.py
+++ tests/test_main.py
@@ -12,27 +12,16 @@ class TestParseData(unittest.TestCase):
         self.marker_dummy = None
         self.bad_marker_dummy = None
 
-    class NoParsedObject(object):
-        """docstring for ClassName"""
-        def __init__(self, arg):
-            self.arg = arg
-
-    def test_class_null(self):
-        self.assertEqual(parse_data(Marker, self.marker_dummy), None)
-
     def test_data_null(self):
-        self.assertEqual(parse_data(Marker, None), None)
+        self.assertIsNone(parse_data(Marker, None))
 
-    def test_bad_data_format_marker_class(self):
-        self.assertEqual(parse_data(Marker, self.bad_marker_dummy), None)
-
-    def test_class_without_parse_method(self):
-        self.assertEqual(parse_data(self.NoParsedObject, self.marker_dummy), None)
+    def test_bad_data(self):
+        self.assertIsNone(parse_data(Marker, self.bad_marker_dummy))
 
     def test_parse_marker(self):
-        marker  = parse_data(Marker, self.marker_dummy)
-        for key in self.marker_dummy:
-            self.assertEqual(marker.__dict__[key], self.marker_dummy[key])
+        marker = parse_data(Marker, self.marker_dummy)
+        for key, value in self.marker_dummy.iteritems():
+            self.assertEqual(marker.getattr(key), value)
 
 if __name__ == '__main__':
     unittest.main()

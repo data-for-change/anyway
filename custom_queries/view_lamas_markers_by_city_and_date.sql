860bd27e2a914d9f15add28b9010dd1c3e40fd47
yitzikc <yitzikc@gmail.com>
2017-06-09 18:32:36 +0300

    View showing LAMAS markers by city and date

---
+++ custom_queries/view_lamas_markers_by_city_and_date.sql
@@ -0,0 +1,11 @@
+﻿CREATE OR REPLACE VIEW
+lamas_markers_by_city_and_date AS
+SELECT
+	id,
+	trim(both '* ' FROM (regexp_split_to_array(address, ',\s*'))[2]) AS city,
+	CAST(date_part('year',  created) AS INTEGER) AS year,
+	CAST(date_part('month', created) AS INTEGER) AS month,
+	CAST(date_part('dow',   created) AS INTEGER) AS day_of_week
+	FROM markers WHERE provider_code = 3 AND address ~ ',';
+
+COMMENT ON COLUMN lamas_markers_by_city_and_date.day_of_week IS 'Sunday = 0 to Saturday = 6';

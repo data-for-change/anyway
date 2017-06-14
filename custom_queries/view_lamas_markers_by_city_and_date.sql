2d7fb0212af47a6340165b864a8528caf4604a39
Yitzik Casapu <yitzikc@gmail.com>
2017-06-14 10:37:12 +0300

    Query city and year (#675)
    * View showing LAMAS markers by city and date
    
    * Add severity to lamas_markers_by_city_and_date view
    
    * Add lamas_marker_counts_by_city_year_and_severity view
    
    * Add weighting by severity
    
    * Add stored procedure for querying YoY deltas
    
    * Allow invocation of accidents_yoy_by_city with no city filter and add documentation
    
    * Add a README for the custom queries subdirectory

---
+++ custom_queries/view_lamas_markers_by_city_and_date.sql
@@ -0,0 +1,34 @@
+﻿CREATE OR REPLACE VIEW
+lamas_markers_by_city_and_date AS
+SELECT
+	id,
+	trim(both '* ' FROM (regexp_split_to_array(address, ',\s*'))[2]) AS city,
+	severity,
+	CAST(date_part('year',  created) AS INTEGER) AS year,
+	CAST(date_part('month', created) AS INTEGER) AS month,
+	CAST(date_part('dow',   created) AS INTEGER) AS day_of_week
+	FROM markers WHERE provider_code = 3 AND address ~ ',';
+
+COMMENT ON COLUMN lamas_markers_by_city_and_date.day_of_week IS 'Sunday = 0 to Saturday = 6';
+
+CREATE OR REPLACE FUNCTION severity_to_weight(INTEGER) RETURNS INTEGER
+AS $$
+BEGIN
+	CASE ($1)
+	WHEN 1 THEN -- Fatal
+		RETURN 7;
+	WHEN 2 THEN -- Serious injury
+		RETURN 5;
+	ELSE
+		RETURN 1;
+	END CASE;
+END
+$$ LANGUAGE plpgsql IMMUTABLE;
+
+CREATE OR REPLACE VIEW
+lamas_marker_counts_by_city_year_and_severity AS
+	SELECT city ,year, severity,
+	COUNT(DISTINCT id) AS count,
+	severity_to_weight(severity) * COUNT(DISTINCT id) AS weighted_count
+	FROM lamas_markers_by_city_and_date
+	GROUP BY city, year, severity;
---
+++ custom_queries/yoy_weighted_count_delta.sql
@@ -0,0 +1,20 @@
+﻿CREATE OR REPLACE FUNCTION accidents_yoy_by_city(TEXT ARRAY)
+RETURNS TABLE(city TEXT, year INTEGER, total_weighted_count NUMERIC, weighted_count_yoy_delta NUMERIC)
+AS $$
+	SELECT
+		city,
+		year,
+		sum(weighted_count) AS total_weighted_count,
+		sum(weighted_count) - lag(sum(weighted_count)) OVER (PARTITION BY city ORDER BY year) AS weighted_count_yoy_delta
+	FROM lamas_marker_counts_by_city_year_and_severity
+	WHERE cardinality($1) = 0 OR city = ANY($1)
+GROUP BY city, year
+$$ LANGUAGE SQL;
+
+COMMENT ON FUNCTION accidents_yoy_by_city(TEXT ARRAY) IS
+'Tabulate the total weighted accident count for the specified cities and its year-on-year difference. When an empty array of cities is specified, give results for all cities';
+
+-- Example invocation with a specific list of cities:
+--SELECT accidents_yoy_by_city(ARRAY[ 'רמת גן', 'חיפה' ]);
+-- Example invocation with no filtering for city name:
+--SELECT accidents_yoy_by_city(ARRAY[]::TEXT[]);

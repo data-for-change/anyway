a165e4826db45f9d6086b23b0cb21b41fd8b1936
Roey Darwish Dror <roey.ghost@gmail.com>
2017-09-13 09:46:39 +0300

    Move load_discussions into main (fixes #730) (#731)
    * Move load_discussions into main (fixes #730)
    
    * Remove 'end=""' in logging calls
    
    * Strip in case using stdin; show error if failed

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
-
-import os
 import click
 import logging
+import os
+import re
+import sys
+
 
 @click.group()
 def cli():
@@ -58,6 +60,37 @@ def import_united_data(light, username, password, lastmail):
     from anyway.united import main
 
     return main(light=light, username=username, password=password, lastmail=lastmail)
+
+
+@cli.command()
+@click.argument('identifiers', nargs=-1)
+def load_discussions(identifiers):
+    from anyway.database import db_session
+    from anyway.models import DiscussionMarker
+
+    identifiers = identifiers or sys.stdin
+
+    for identifier in identifiers:
+        identifier = identifier.strip()
+        m = re.match(r'\((\d+\.\d+),\s*(\d+\.\d+)\)', identifier)
+        if not m:
+            logging.error("Failed processing: " + identifier)
+            continue
+        (latitude, longitude) = m.group(1, 2)
+        marker = DiscussionMarker.parse({
+            'latitude': latitude,
+            'longitude': longitude,
+            'title': identifier,
+            'identifier': identifier
+        })
+        try:
+            db_session.add(marker)
+            db_session.commit()
+            logging.info("Added:  " + identifier)
+        except Exception as e:
+            db_session.rollback()
+            logging.warn("Failed: " + identifier + ": " + e.message)
+
 
 if __name__ == '__main__':
     cli()

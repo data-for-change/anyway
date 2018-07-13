4f39a4e7812abb42ecd6e7c831ea94c003371e40
atalyaalon <atalya.alon@gmail.com>
2018-07-13 19:43:39 +0300

    Update 5a5ffe56bb7_adding_geom_table_to_markers_and_discussions.py

--- alembic/versions/5a5ffe56bb7_adding_geom_table_to_markers_and_discussions.py
+++ alembic/versions/5a5ffe56bb7_adding_geom_table_to_markers_and_discussions.py
@@ -5,8 +5,6 @@ Revises: 3c0d35fdbe2e
 Create Date: 2018-03-07 13:49:06.780319
 
 """
-from geoalchemy2.types import Geometry
-
 # revision identifiers, used by Alembic.
 revision = '5a5ffe56bb7'
 down_revision = '3c0d35fdbe2e'
@@ -15,6 +13,7 @@ depends_on = None
 
 from alembic import op
 import sqlalchemy as sa
+import geoalchemy2 as ga
 
 
 def upgrade():
@@ -23,6 +22,7 @@ def upgrade():
     conn.execution_options(isolation_level="AUTOCOMMIT")
     conn.execute('CREATE EXTENSION IF NOT EXISTS postgis;')
     conn.execute('CREATE EXTENSION IF NOT EXISTS postgis_topology;')
+    op.add_column('markers', sa.Column(sa.Column('geom', ga.Geometry('POINT', srid=4326)))
     conn.execute("SELECT AddGeometryColumn('public','markers','geom',4326,'POINT',2);")
     conn.execute('UPDATE markers SET geom = ST_SetSRID(ST_MakePoint(longitude,latitude),4326);')
     conn.execute('CREATE INDEX idx_markers_geom ON markers USING GIST(geom);')

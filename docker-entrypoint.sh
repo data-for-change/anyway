d0fae1c46c2a9cad052f8b21843e0c73f5b2dcbb
Ori Hoch <ori@uumpa.com>
2018-11-05 21:19:56 +0200

    docker fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -u
 
-mv /rsa.xlsx /anyway/static/data/rsa/
-
 sleep 2
 ! alembic upgrade head && sleep 2 && ! alembic upgrade head && sleep 2 \
 && ! alembic upgrade head && echo failed to upgrade head && exit 1
 
+ls -lah /rsa.xlsx
+
 python main.py process cbs &&\
-python main.py process rsa static/data/rsa/rsa.xlsx
+python main.py process rsa /rsa.xlsx
 [ "$?" != "0" ] && echo failed to import data && exit 1
 
 exec "$@"

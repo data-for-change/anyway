ac7e8bec0fed095c3e94175bda92a79a6a3cb031
atalyaalon <atalya.alon@gmail.com>
2019-01-12 18:55:49 +0200

    Update flask_app.py

--- anyway/flask_app.py
+++ anyway/flask_app.py
@@ -61,16 +61,15 @@ app.config['OAUTH_CREDENTIALS'] = {
         'secret': os.environ.get('GOOGLE_LOGIN_CLIENT_SECRET')
     }
 }
-# temp - disable graphql
-# app.add_url_rule(
-#     '/graphql',
-#     view_func=GraphQLView.as_view(
-#         'graphql',
-#         schema=graphqlSchema,
-#         graphiql=True,
-#         get_context=lambda: {'session': db.session}
-#     )
-# )
+app.add_url_rule(
+    '/graphql',
+    view_func=GraphQLView.as_view(
+        'graphql',
+        schema=graphqlSchema,
+        graphiql=True,
+        get_context=lambda: {'session': db.session}
+    )
+)
 assets = Environment()
 assets.init_app(app)
 

625d560081cddc6a22f3c8b473006d00f29c8414
Mickey Rouash <mickey.rouash@gmail.com>
2019-03-04 20:01:46 +0200

    update it for full db migration

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-set -u
 
-sleep 2
-! alembic upgrade head && sleep 2 && ! alembic upgrade head && sleep 2 \
-&& ! alembic upgrade head && echo failed to upgrade head && exit 1
 
-ls -lah /rsa.xlsx
+if [ "${INITIALIZE}" == "yes" ]; then
+  sleep 2
+  ! alembic upgrade head && sleep 2 && ! alembic upgrade head && sleep 2 \
+  && ! alembic upgrade head && echo failed to upgrade head && exit 1
+fi  
 
-python main.py process cbs &&\
-python main.py process rsa /rsa.xlsx
-[ "$?" != "0" ] && echo failed to import data && exit 1
+#ls -lah /rsa.xlsx
+
+#python main.py process cbs &&\
+#python main.py process rsa /rsa.xlsx
+#[ "$?" != "0" ] && echo failed to import data && exit 1
 
 exec "$@"

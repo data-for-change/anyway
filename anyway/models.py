6474b0dd81bd275d2391e209b910342aeba91e92
Adva Klinger <advaklinger@gmail.com>
2019-09-09 20:54:04 +0300

    add report_problem API for mobile
    Signed-off-by: Adva Klinger <advaklinger@gmail.com>

--- anyway/models.py
+++ anyway/models.py
@@ -1688,3 +1688,27 @@ class RoadSegments(Base):
     from_name = Column(Text())
     to_km = Column(Float())
     to_name = Column(Text())
+
+
+class ReportProblem(Base):
+    __tablename__ = "report_problem"
+    id = Column(BigInteger(), autoincrement=True, primary_key=True, index=True)
+    latitude = Column(Float())
+    longitude = Column(Float())
+    problem_description = Column(Text())
+    signs_on_the_road_not_clear = Column(Boolean())
+    signs_problem = Column(Boolean())
+    pothole = Column(Boolean())
+    no_light = Column(Boolean())
+    no_sign = Column(Boolean())
+    crossing_missing = Column(Boolean())
+    sidewalk_is_blocked = Column(Boolean())
+    street_light_issue = Column(Boolean())
+    road_hazard = Column(Boolean())
+    first_name = Column(String(50))
+    last_name = Column(String(50))
+    phone_number = Column(String(50))
+    email = Column(String(100))
+    send_to_municipality = Column(Boolean())
+    image_data = Column(String())
+    personal_id = Column(String(20))

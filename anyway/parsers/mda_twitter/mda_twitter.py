49739dfc3d9047b8b67fcf5f00b9084f4e52f6c1
sagiv harel <sagivh203@gmail.com>
2019-11-16 21:47:19 +0200

    update classify tweets function

--- anyway/parsers/mda_twitter/mda_twitter.py
+++ anyway/parsers/mda_twitter/mda_twitter.py
@@ -26,21 +26,12 @@ def classify_tweets(text):
     :param text: tweet text
     :return: boolean, true if tweet is about car accident, false for others
     """
-    if ((u'תאונ' in text and u'תאונת עבודה' not in text and u'תאונות עבודה' not in text)
-        or ((u'רכב' in text or u'אוטובוס' in text or u"ג'יפ" in text
-             or u'משאית' in text or u'קטנוע' in text or u'טרקטור'
-             in text or u'אופנוע' in text or u'אופניים' in text or u'קורקינט'
-             in text or u'הולך רגל' in text or u'הולכת רגל' in text
-             or u'הולכי רגל' in text) and
-            (u'פגע' in text or u'פגיע' in text or u'פגוע' in text or
-             u'הרג' in text or u'הריג' in text or u'הרוג' in text or
-             u'פצע' in text or u'פציע' in text or u'פצוע' in text or
-             text or u'התנגש' in text or u'התהפך'
-             in text or u'התהפכ' in text))) and \
-            (u' ירי ' not in text and not text.startswith(u' ירי') and
-             u' ירייה ' not in text and not text.startswith(u' ירייה') and
-             u' יריות ' not in text and not text.startswith(u' יריות')):
-        return True
+    if text.startswith(u'בשעה') and \
+        ((u'הולך רגל' in text or u'הולכת רגל' in text or u'נהג' in text or u'אדם' in text) and \
+            (u'רכב' in text or u'מכונית' in text or u'אופנוע' in text or u"ג'יפ" in text or u'טרקטור' in text or u'משאית' in text or \
+                 u'אופניים' in text or u'קורקינט' in text)) or \
+        (u'תאונה' in text or u'תאונת דרכים' in text):
+            return True
     return False
 
 

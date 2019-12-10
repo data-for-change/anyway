0061caac8e50866d9446ab4f3fc0dec2a354c74a
Atalya Alon <atalya.alon@gmail.com>
2019-12-10 12:58:25 +0200

    Fix dictionaries in /markers/all query

--- anyway/flask_app.py
+++ anyway/flask_app.py
@@ -607,35 +607,39 @@ def involved_data_refinement(involved):
 def get_involved_dict(provider_code, accident_year):
     involved = {}
     age_group = db.session.query(AgeGroup).filter(and_(AgeGroup.provider_code == provider_code,
-                                                       AgeGroup.year == accident_year)).first()
-    involved["age_group"] = age_group.age_group_hebrew if age_group else None
+                                                       AgeGroup.year == accident_year)).all()
+    involved["age_group"] = {g.id: g.age_group_hebrew for g in age_group} if age_group else None
 
     population_type = db.session.query(PopulationType).filter(and_(PopulationType.provider_code == provider_code,
-                                                                   PopulationType.year == accident_year)).first()
+                                                                   PopulationType.year == accident_year)).all()
 
-    involved["population_type"] = population_type.population_type_hebrew if population_type else None
+    involved["population_type"] = {g.id: g.population_type_hebrew for g in population_type} if population_type else None
 
     home_region = db.session.query(Region).filter(and_(Region.provider_code == provider_code,
-                                                       Region.year == accident_year)).first()
-    involved["home_region"] = home_region.region_hebrew if home_region else None
+                                                       Region.year == accident_year)).all()
+
+    involved["home_region"] = {g.id: g.region_hebrew for g in home_region} if home_region else None
 
     home_district = db.session.query(District).filter(and_(District.provider_code == provider_code,
-                                                           District.year == accident_year)).first()
-    involved["home_district"] = home_district.district_hebrew if home_district else None
+                                                           District.year == accident_year)).all()
+
+    involved["home_district"] = {g.id: g.district_hebrew for g in home_district} if home_district else None
 
     home_natural_area = db.session.query(NaturalArea).filter(and_(NaturalArea.provider_code == provider_code,
-                                                                  NaturalArea.year == accident_year)).first()
-    involved["home_natural_area"] = home_natural_area.natural_area_hebrew if home_natural_area else None
+                                                                  NaturalArea.year == accident_year)).all()
+
+    involved["home_natural_area"] = {g.id: g.natural_area_hebrew for g in home_natural_area} if home_natural_area else None
 
     home_municipal_status = db.session.query(MunicipalStatus).filter(
         and_(MunicipalStatus.provider_code == provider_code,
-             MunicipalStatus.year == accident_year)).first()
+             MunicipalStatus.year == accident_year)).all()
     involved[
-        "home_municipal_status"] = home_municipal_status.municipal_status_hebrew if home_municipal_status else None
+        "home_municipal_status"] = {g.id: g.municipal_status_hebrew for g in home_municipal_status} if home_municipal_status else None
 
     home_yishuv_shape = db.session.query(YishuvShape).filter(and_(YishuvShape.provider_code == provider_code,
-                                                                  YishuvShape.year == accident_year)).first()
-    involved["home_yishuv_shape"] = home_yishuv_shape.yishuv_shape_hebrew if home_yishuv_shape else None
+                                                                  YishuvShape.year == accident_year)).all()
+
+    involved["home_yishuv_shape"] = {g.id: g.yishuv_shape_hebrew for g in home_yishuv_shape} if home_yishuv_shape else None
 
     return involved
 
@@ -643,19 +647,20 @@ def get_involved_dict(provider_code, accident_year):
 def get_vehicle_dict(provider_code, accident_year):
     vehicle = {}
     engine_volume = db.session.query(EngineVolume) \
-        .filter(and_(EngineVolume.provider_code == provider_code, EngineVolume.year == accident_year)) \
-        .first()
-    vehicle["engine_volume"] = engine_volume.engine_volume_hebrew if engine_volume else None
+        .filter(and_(EngineVolume.provider_code == provider_code, EngineVolume.year == accident_year))\
+        .all()
+
+    vehicle["engine_volume"] = {g.id: g.engine_volume_hebrew for g in engine_volume} if engine_volume else None
 
     total_weight = db.session.query(TotalWeight) \
         .filter(and_(TotalWeight.provider_code == provider_code, TotalWeight.year == accident_year)) \
-        .first()
-    vehicle["total_weight"] = total_weight.total_weight_hebrew if engine_volume else None
+        .all()
+    vehicle["total_weight"] = {g.id: g.total_weight_hebrew for g in total_weight} if total_weight else None
 
     driving_directions = db.session.query(DrivingDirections) \
         .filter(and_(DrivingDirections.provider_code == provider_code, DrivingDirections.year == accident_year)) \
-        .first()
-    vehicle["driving_directions"] = driving_directions.driving_directions_hebrew if engine_volume else None
+        .all()
+    vehicle["driving_directions"] = {g.id: g.driving_directions_hebrew for g in driving_directions} if driving_directions else None
 
     return vehicle
 
@@ -705,22 +710,22 @@ def marker_all():
     for inv in involved:
         obj = inv.serialize()
         new_inv = get_involved_dict(provider_code, accident_year)
-        obj["age_group"] = new_inv["age_group"]
-        obj["population_type"] = new_inv["population_type"]
-        obj["home_region"] = new_inv["home_region"]
-        obj["home_district"] = new_inv["home_district"]
-        obj["home_natural_area"] = new_inv["home_natural_area"]
-        obj["home_municipal_status"] = new_inv["home_municipal_status"]
-        obj["home_yishuv_shape"] = new_inv["home_yishuv_shape"]
+        obj["age_group"] = new_inv["age_group"].get(obj["age_group"]) if new_inv["age_group"] else None
+        obj["population_type"] = new_inv["population_type"].get(obj["population_type"]) if new_inv["population_type"] else None
+        obj["home_region"] = new_inv["home_region"].get(obj["home_region"]) if new_inv["home_region"] else None
+        obj["home_district"] = new_inv["home_district"].get(obj["home_district"]) if new_inv["home_district"] else None
+        obj["home_natural_area"] = new_inv["home_natural_area"].get(obj["home_natural_area"]) if new_inv["home_natural_area"] else None
+        obj["home_municipal_status"] = new_inv["home_municipal_status"].get(obj["home_municipal_status"]) if new_inv["home_municipal_status"] else None
+        obj["home_yishuv_shape"] = new_inv["home_yishuv_shape"].get(obj["home_yishuv_shape"]) if new_inv["home_yishuv_shape"] else None
 
         list_to_return.append(obj)
 
     for veh in vehicles:
         obj = veh.serialize()
         new_veh = get_vehicle_dict(provider_code, accident_year)
-        obj["engine_volume"] = new_veh["engine_volume"]
-        obj["total_weight"] = new_veh["total_weight"]
-        obj["driving_directions"] = new_veh["driving_directions"]
+        obj["engine_volume"] = new_veh["engine_volume"].get(obj["engine_volume"]) if new_veh["engine_volume"] else None
+        obj["total_weight"] = new_veh["total_weight"].get(obj["total_weight"]) if new_veh["total_weight"] else None
+        obj["driving_directions"] = new_veh["driving_directions"].get(obj["driving_directions"]) if new_veh["driving_directions"] else None
 
         list_to_return.append(obj)
     return make_response(json.dumps(list_to_return, ensure_ascii=False))

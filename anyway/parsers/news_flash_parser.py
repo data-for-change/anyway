6d4048606cb1e56a93d7a1cfa2858f001c50bb10
sagiv harel <sagivh203@gmail.com>
2020-03-16 21:35:21 +0200

    fixed minor comma issue

--- anyway/parsers/news_flash_parser.py
+++ anyway/parsers/news_flash_parser.py
@@ -112,7 +112,7 @@ def insert_new_flash_news(title, link, date_parsed, author, description, locatio
     db.session.execute('INSERT INTO news_flash (tweet_id, title, link, date, author, description, location, lat, lon, '
                        'resolution, region_hebrew, district_hebrew, yishuv_name, street1_hebrew, street2_hebrew, '
                        'non_urban_intersection_hebrew, road1, road2, road_segment_name, '
-                       'accident, tags source'') VALUES \
+                       'accident, tags, source'') VALUES \
                        (:tweet_id, :title, :link, :date, :author, :description, :location, :lat, :lon, :resolution, \
                        :region_hebrew, :district_hebrew, :yishuv_name, :street1_hebrew, :street2_hebrew,'
                        ' :non_urban_intersection_hebrew, :road1, :road2, :road_segment_name,'

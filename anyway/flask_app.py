d8a02c3f9f1ed2427c754e325966be13446f7400
Atalya Alon <20992625+atalyaalon@users.noreply.github.com>
2020-03-17 09:52:03 +0200

    change widget accidents_count_by_hour name

--- anyway/flask_app.py
+++ anyway/flask_app.py
@@ -1636,9 +1636,9 @@ def infographics_data():
     output['widgets'].append(accident_count_by_day_night)
 
     # accidents distribution count by hour
-    accidents_distribution_count_by_hour = {'name': 'accidents_distribution_count_by_hour',
+    accidents_count_by_hour = {'name': 'accidents_distribution_count_by_hour',
                                             'data': get_accidents_stats(table_obj=AccidentMarkerView, filters=location_info, group_by='accident_hour', count='accident_hour', start_time=start_time, end_time=end_time),
                                             'meta': {}}
-    output['widgets'].append(accidents_distribution_count_by_hour)
+    output['widgets'].append(accidents_count_by_hour)
 
     return Response(json.dumps(output, default=str), mimetype="application/json")

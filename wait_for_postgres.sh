e474b59c081698117549edf699e08067f8d496fd
ItamarLevin <Itamarlevin94@gmail.com>
2020-04-15 00:03:35 +0300

    fixing GitHub Action CI

--- wait_for_postgres.sh
+++ wait_for_postgres.sh
@@ -1,6 +1,6 @@
 RETRIES=60
 export PGPASSWORD=anyway
-until psql -h db -U anyway -d anyway -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
+until psql -h db -U anyway -d anywaya -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
   echo "Waiting for postgres server, $((RETRIES--)) remaining attempts..."
   sleep 1
 done

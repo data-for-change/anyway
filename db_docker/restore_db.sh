3cf1fd6055518573f30e800618489e17c3ef3f9a
Dima Kuznetsov <dmitrykuzn@gmail.com>
2020-06-03 18:13:19 +0000

    Enable downloading dump on DB startup
    This patch adds dump dowload stage to container startup if dump is missing
    but GDRIVE_FILE_ID is provided and RESTORE_DB is set to TRUE

--- db_docker/restore_db.sh
+++ db_docker/restore_db.sh
@@ -5,6 +5,10 @@ set -x
 
 if [ "$RESTORE_DB" == "TRUE" ]; then
     echo "******DEV Env - PostgreSQL initialisation******"
+    if [ ! -f $DB_DUMP_PATH ]
+    then
+        python3 /download_dump.py
+    fi
     pg_restore -Fc "$DB_DUMP_PATH" -d "$POSTGRES_DB" --no-owner
 else
     echo "Not DEV environment, not restoring db"

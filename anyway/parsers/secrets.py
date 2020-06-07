0e65acf851ce949f1628dac96c9f50c7a28d509d
Elazar Gershuni <elazarg@gmail.com>
2020-06-07 19:22:38 +0300

    nicer secret handling

--- anyway/parsers/secrets.py
+++ anyway/parsers/secrets.py
@@ -1,18 +1,27 @@
 import os
+from functools import lru_cache
 import logging
 
 # Never log a secret key!
 # Note that github-secrets are not accessible for PRs from forks.
 
+SECRETS_DIR = "/run/secrets/"
+IS_SECRET_DIR = os.path.isdir(SECRETS_DIR)
 
-def get(secret_name: str) -> str:
-    logging.info("Reading secret for " + repr(secret_name))
-    secrets_dir = "/run/secrets/"
-    if os.path.isdir(secrets_dir):
-        with open(secrets_dir + secret_name) as secret_file:
+if IS_SECRET_DIR:
+
+    @lru_cache(10, typed=True)
+    def get(secret_name: str) -> str:
+        logging.info("Reading secret for " + repr(secret_name))
+        with open(SECRETS_DIR + secret_name) as secret_file:
             return secret_file.read()
-    logging.info("No secret dir found.")
-    return os.environ[secret_name]
+
+
+else:
+
+    def get(secret_name: str) -> str:
+        logging.info("Reading secret for " + repr(secret_name))
+        return os.environ[secret_name]
 
 
 def exists(secret_name: str) -> bool:

da67f90b3b049f4fc34cfda84d1633f87f4eabe8
David Shemian <davidshemian18@gmail.com>
2020-06-13 13:54:53 +0300

    working on the tests

--- tests/test_infographic_api.py
+++ tests/test_infographic_api.py
@@ -2,6 +2,7 @@ import pytest
 import six
 from six.moves import http_client
 from anyway import app as flask_app
+from jsonschema import validate
 
 
 @pytest.fixture
@@ -47,8 +48,8 @@ class TestInfographicApi:
             "road1": 90.0,
             "road_segment_name": "כניסה למצפה שלם - צומת שדי תרומות"
         }
+    # cd tests && python -m pytest -s test_infographic_api.py
 
-    # python -m pytest -s test_infographic_api.py
     def test_infographic_with_existing_news_flash(self, widgets):
         self.accident_count_by_severity_test(widgets)
         self.most_severe_accidents_table_test(widgets)
@@ -56,6 +57,14 @@ class TestInfographicApi:
         self.street_view_test(widgets)
         self.head_on_collisions_comparison_test(widgets)
         self.accident_count_by_accident_type_test(widgets)
+        self.accidents_heat_map_test(widgets)
+        self.accident_count_by_accident_year_test(widgets)
+        self.injured_count_by_accident_year_test(widgets)
+        self.accident_count_by_day_night_test(widgets)
+        self.accidents_count_by_hour_test(widgets)
+        self.accident_count_by_road_light_test(widgets)
+        self.top_road_segments_accidents_per_km_test(widgets)
+        self.injured_count_per_age_group_test(widgets)
 
     def accident_count_by_severity_test(self, widgets):
         assert widgets[0]['name'] == 'accident_count_by_severity'
@@ -106,37 +115,173 @@ class TestInfographicApi:
     def head_on_collisions_comparison_test(self, widgets):
         assert widgets[4]['name'] == 'head_on_collisions_comparison'
 
-        specific_road_segment_fatal_accidents_example = {
-            "desc": "תאונות אחרות",
-            "count": 5
-        }
-
-        all_roads_fatal_accidents_example = {
-            "desc": "התנגשות חזית בחזית",
-            "count": 195
+        schema = {
+            "type": "object",
+            "properties": {
+                "desc": {"type": "string"},
+                "count": {"type": "number"},
+            },
         }
 
         items = widgets[4]['data']['items']
 
-        assert specific_road_segment_fatal_accidents_example in items['specific_road_segment_fatal_accidents']
-        assert all_roads_fatal_accidents_example in items['all_roads_fatal_accidents']
+        validate(items['specific_road_segment_fatal_accidents'][0], schema)
+        validate(items['all_roads_fatal_accidents'][0], schema)
 
     def accident_count_by_accident_type_test(self, widgets):
         assert widgets[5]['name'] == 'accident_count_by_accident_type'
         assert len(widgets[5]['data']['items']) > 6
 
-        example_1 = {
-            "accident_type": "התנגשות",
-            "count": 190
+        schema = {
+            "type": "object",
+            "properties": {
+                "accident_type": {"type": "string"},
+                "count": {"type": "number"},
+            },
         }
 
-        example_2 = {
-            "accident_type": "נפילה מרכב נע",
-            "count": 1
+        validate(widgets[5]['data']['items'][0], schema)
+
+    def accidents_heat_map_test(self, widgets):
+        assert widgets[6]['name'] == 'accidents_heat_map'
+        assert len(widgets[6]['data']['items']) > 250
+
+        example = [
+            {
+                "longitude": 35.4694113582293,
+                "latitude": 31.9741018651507
+            },
+            {
+                "longitude": 35.4699271070643,
+                "latitude": 31.9400753723359
+            },
+            {
+                "longitude": 35.4144223319655,
+                "latitude": 31.6302170694553
+            },
+            {
+                "longitude": 35.543722222328,
+                "latitude": 32.3175046540988
+            }
+        ]
+
+        assert all(element in widgets[6]['data']['items']
+                   for element in example)
+
+    def accident_count_by_accident_year_test(self, widgets):
+        assert widgets[7]['name'] == 'accident_count_by_accident_year'
+        assert len(widgets[7]['data']['items']) > 4
+
+        schema = {
+            "type": "object",
+            "properties": {
+                "accident_year": {"type": "number"},
+                "count": {"type": "number"},
+            },
         }
 
-        assert example_1 in widgets[5]['data']['items']
-        assert example_2 in widgets[5]['data']['items']
+        validate(widgets[7]['data']['items'][0], schema)
+        assert widgets[7]['data']['text']['title'] == 'כמות תאונות'
+
+    def injured_count_by_accident_year_test(self, widgets):
+        assert widgets[8]['name'] == 'injured_count_by_accident_year'
+        assert len(widgets[8]['data']['items']) > 4
+
+        schema = {
+            "type": "object",
+            "properties": {
+                "accident_year": {"type": "number"},
+                "count": {"type": "number"},
+            },
+        }
+
+        validate(widgets[8]['data']['items'][0], schema)
+        assert widgets[8]['data']['text']['title'] == 'כמות פצועים'
+
+    def accident_count_by_day_night_test(self, widgets):
+        assert widgets[9]['name'] == 'accident_count_by_day_night'
+        assert len(widgets[9]['data']['items']) > 1
+
+        schema = {
+            "type": "object",
+            "properties": {
+                "day_night": {"type": "string"},
+                "count": {"type": "number"},
+            },
+        }
+
+        validate(widgets[9]['data']['items'][0], schema)
+        assert widgets[9]['data']['text']['title'] == 'כמות תאונות ביום ובלילה'
+
+    def accidents_count_by_hour_test(self, widgets):
+        assert widgets[10]['name'] == 'accidents_count_by_hour'
+        assert len(widgets[10]['data']['items']) > 20
+
+        schema = {
+            "type": "object",
+            "properties": {
+                "accident_hour": {"type": "number"},
+                "count": {"type": "number"},
+            },
+        }
+
+        validate(widgets[10]['data']['items'][0], schema)
+        assert widgets[10]['data']['text']['title'] == 'כמות תאונות לפי שעה'
+
+    def accident_count_by_road_light_test(self, widgets):
+        assert widgets[11]['name'] == 'accident_count_by_road_light'
+        assert len(widgets[11]['data']['items']) > 6
+        schema = {
+            "type": "object",
+            "properties": {
+                "road_light": {"type": "string"},
+                "count": {"type": "number"},
+            },
+        }
+
+        validate(widgets[11]['data']['items'][0], schema)
+        assert widgets[11]['data']['text']['title'] == 'כמות תאונות לפי תאורה'
+
+    def top_road_segments_accidents_per_km_test(self, widgets):
+        assert widgets[12]['name'] == 'top_road_segments_accidents_per_km'
+        assert len(widgets[12]['data']['items']) > 4
+
+        schema = {
+            "road_segment_name": {"type": "string"},
+            "total_accidents": {"type": "number"},
+            "segment_length": {"type": "number"},
+            "accidents_per_km": {"type": "number"}
+        }
+
+    def injured_count_per_age_group_test(self, widgets):
+        assert widgets[13]['name'] == 'injured_count_per_age_group'
+
+        schema = [
+            {
+                "age_group": "00-14",
+                "count": {"type": "number"}
+            },
+            {
+                "age_group": "15-24",
+                "count": {"type": "number"}
+            },
+            {
+                "age_group": "25-64",
+                "count": {"type": "number"}
+            },
+            {
+                "age_group": "unknown",
+                "count": {"type": "number"}
+            },
+            {
+                "age_group": "65+",
+                "count": {"type": "number"}
+            }
+        ]
+
+        items = widgets[13]['data']['items']
+        for i, s in zip(items, schema):
+            validate(i, s)
 
     # def insert_infographic_mock_data(self, app):
     #     sql_insert = '''

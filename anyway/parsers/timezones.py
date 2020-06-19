83009a3d7c8cf19958a4529ca2f342c485d693ce
Elazar Gershuni <elazarg@gmail.com>
2020-06-19 19:43:25 +0300

    Fix incorrect loading of latest date (#1364)
    * fix incorrect loading of latest date
    
    * fix incorrect expected time in test
    
    * assert tz-aware; skip when new == maxold

--- anyway/parsers/timezones.py
+++ anyway/parsers/timezones.py
@@ -3,6 +3,10 @@ from datetime import datetime, timezone, timedelta
 ISREAL_SUMMER_TIMEZONE = timezone(offset=timedelta(hours=3))
 
 
+def is_tz_aware(dt):
+    return dt.tzinfo.utcoffset(dt) is not None and dt.tzinfo is not None
+
+
 def parse_creation_datetime(raw_date: str):
     """Unified time format detection and parsing.
 
@@ -12,17 +16,21 @@ def parse_creation_datetime(raw_date: str):
     walla: 'Sun, 31 May 2020 08:26:18 GMT' or like ynet
     """
     try:
-        # +0300 (ynet or walla)
-        return datetime.strptime(raw_date, "%a, %d %b %Y %H:%M:%S %z").replace(
-            tzinfo=ISREAL_SUMMER_TIMEZONE
-        )
-    except ValueError as ex:
-        print(ex)
+        # +0200 or +0300 depending on daylight saving time (ynet or walla)
+        time = datetime.strptime(raw_date, "%a, %d %b %Y %H:%M:%S %z")
+    except ValueError:
         try:
             # +0000 (twitter)
             time = datetime.strptime(raw_date, "%a %b %d %H:%M:%S %z %Y")
-        except ValueError as ex:
-            print(ex)
+        except ValueError:
             # GMT (walla)
             time = datetime.strptime(raw_date, "%a, %d %b %Y %H:%M:%S %Z")
-        return time.replace(tzinfo=timezone.utc).astimezone(tz=ISREAL_SUMMER_TIMEZONE)
+            time = time.replace(tzinfo=timezone.utc)
+    assert is_tz_aware(time)
+    time = time.astimezone(tz=ISREAL_SUMMER_TIMEZONE)
+    return time
+
+
+def from_db(date):
+    # DB holds timezone as UTC; to get local-timezone pretty-printing, we change it on load time
+    return date.astimezone(tz=ISREAL_SUMMER_TIMEZONE)

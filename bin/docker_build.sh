a4ad5f8d02b60c8b143fa576ab67ce0a104834d9
meirg <israeisraeli0@gmail.com>
2020-09-21 16:16:43 +0300

    fixed bug, add all the real code

--- bin/docker_build.sh
+++ bin/docker_build.sh
@@ -1,17 +1,15 @@
 # !/usr/bin/env bash
 
-echo "docker_build.sh"
+ANYWAY_IMG_URL="${SERVER}/${DOCKER_REPOSITORY_ANYWAY}"
+NGINX_IMG_URL"${SERVER}/${DOCKER_REPOSITORY_NGINX}"
 
-# ANYWAY_IMG_URL="${SERVER}/${DOCKER_REPOSITORY_ANYWAY}"
-# NGINX_IMG_URL"${SERVER}/${DOCKER_REPOSITORY_NGINX}"
+echo ANYWAY_IMG_URL="${SERVER}/${DOCKER_REPOSITORY_ANYWAY}" &&\
+echo NGINX_IMG_URL"${SERVER}/${DOCKER_REPOSITORY_NGINX}" &&\
 
-# echo ANYWAY_IMG_URL="${SERVER}/${DOCKER_REPOSITORY_ANYWAY}" &&\
-# echo NGINX_IMG_URL"${SERVER}/${DOCKER_REPOSITORY_NGINX}" &&\
-
-# docker pull "${ANYWAY_IMG_URL}:${SHA_TAG}" &&\
-# if docker pull "${NGINX_IMG_URL}:${REF_TAG}"; then
-#     CACHE_FROM=" --cache-from ${NGINX_IMG_URL}:${REF_TAG} "
-# else
-#     CACHE_FROM=""
-# fi &&\
-# docker build $CACHE_FROM -t "${NGINX_IMG_URL}:${SHA_TAG}" nginx_docker
+docker pull "${ANYWAY_IMG_URL}:${SHA_TAG}" &&\
+if docker pull "${NGINX_IMG_URL}:${REF_TAG}"; then
+    CACHE_FROM=" --cache-from ${NGINX_IMG_URL}:${REF_TAG} "
+else
+    CACHE_FROM=""
+fi &&\
+docker build $CACHE_FROM -t "${NGINX_IMG_URL}:${SHA_TAG}" nginx_docker
--- bin/docker_push_sha.sh
+++ bin/docker_push_sha.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-echo "docker_push_sha.sh"
+IMG_URL="${SERVER}/${DOCKER_REPOSITORY_NGINX}" &&\
 
-# IMG_URL="${SERVER}/${DOCKER_REPOSITORY_NGINX}" &&\
-
-# docker tag "${IMG_URL}:${SHA_TAG}" "${IMG_URL}:${REF_TAG}" &&\
-# docker push "${IMG_URL}:${SHA_TAG}" &&\
-# docker push "${IMG_URL}:${REF_TAG}"
+docker tag "${IMG_URL}:${SHA_TAG}" "${IMG_URL}:${REF_TAG}" &&\
+docker push "${IMG_URL}:${SHA_TAG}" &&\
+docker push "${IMG_URL}:${REF_TAG}"

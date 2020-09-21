2b53ca5bb4745e94a60a4e39e3b5ffe96f11bb56
meirg <israeisraeli0@gmail.com>
2020-09-21 17:41:20 +0300

    add DEBUG mode

--- bin/docker_push_sha.sh
+++ bin/docker_push_sha.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-IMG_URL="${SERVER}/${DOCKER_REPOSITORY_NGINX}" &&\
+IMG_URL="${SERVER}/${DOCKER_REPOSITORY_NGINX}"
 
 docker tag "${IMG_URL}:${SHA_TAG}" "${IMG_URL}:${REF_TAG}" &&\
 docker push "${IMG_URL}:${SHA_TAG}" &&\

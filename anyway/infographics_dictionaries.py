d8078679fa16bdf36b513860fc738d377f9175bc
galrou <gal.rouash@gmail.com>
2020-10-04 01:18:54 +0300

    changed text4

--- anyway/infographics_dictionaries.py
+++ anyway/infographics_dictionaries.py
@@ -1,6 +1,6 @@
 driver_type_hebrew_dict = dict(
     professional_driver="נהג מקצועי", private_vehicle_driver="נהג פרטי", other_driver="לא ידוע"
 )
-collision_type_hebrew_dict=dict(
-    head_to_head_collision="התנגשות חזית בחזית"
+head_on_collisions_comparison_dict=dict(
+    head_to_head_collision="התנגשות חזית בחזית", others="אחרות",head_to_head="חזיתיות"
 )

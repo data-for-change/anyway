bd74cd4d5fbb8eefc2d7d64b58d896ca3ee69784
BarVolunteering <58947331+BarVolunteering@users.noreply.github.com>
2020-10-04 13:51:13 +0300

    Add the real code of accident percentage comparison per vehicle type (#1427)
    * Add the real code of accident percentage comparison per vehicle type in infographics_data
    
    * Fix flask assets clean issue
    
    * Add the real code of accident percentage comparison per vehicle type in infographics_data
    
    * Fix flask assets clean issue
    
    * Round up total percentage to 100% in percentage_accidents_by_car_type
    
    * Add tests for accident_count_by_car_type widget
    
    * Remove empty line
    
    * Revert "Round up total percentage to 100% in percentage_accidents_by_car_type"
    
    This reverts commit a092bafa
    
    * Change backend vehicle type grouping
    
    * Return percentage as a float
    
    * Fix tests
    
    * Change variables names and add fraction approximation

--- anyway/backend_constants.py
+++ anyway/backend_constants.py
@@ -17,6 +17,13 @@ class BackEndConstants(object):
     LIGHT_ELECTRIC_VEHICLE_TYPES = [21, 22, 23]
     OTHER_VEHICLES_TYPES = [15, 16, 17]
 
+    # For percentage_accidents_by_car_type function
+    CAR_VEHICLE_TYPES = [1, 12]
+    LARGE_VEHICLE_TYPES = [25, 11, 4, 18, 6, 2, 3, 5, 13, 7, 24, 14]
+    MOTORCYCLE_VEHICLE_TYPES = [8, 10, 19, 9]
+    BICYCLE_AND_SMALL_MOTOR_VEHICLE_TYPES = [15, 21, 23]
+    OTHER_VEHICLE_TYPES = [14, 17, 22, 16]
+
     AGE_GROUPS_NUMBER = 18
 
     ALL_AGE_GROUPS_LIST = list(range(1, AGE_GROUPS_NUMBER + 1)) + [99]

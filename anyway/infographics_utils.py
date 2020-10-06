c6ba221a14938ecc392ceb0e79a5900df16829dd
Atalya Alon <20992625+atalyaalon@users.noreply.github.com>
2020-10-07 02:19:05 +0300

    Update infographics_utils.py

--- anyway/infographics_utils.py
+++ anyway/infographics_utils.py
@@ -878,7 +878,7 @@ def create_infographics_data(news_flash_id, number_of_years_ago):
         name="accident_severity_by_cross_location",
         rank=19,
         items=accident_severity_by_cross_location_mock_data(),
-        text={"title": "אחוז הפצועים וההרוגים לפי מיקום החציה - בן יהודה תל אביב"}
+        text={"comment": "בן יהודה תל אביב בין השנים 2008-2020"}
     )
     output["widgets"].append(accident_severity_by_cross_location.serialize())
 

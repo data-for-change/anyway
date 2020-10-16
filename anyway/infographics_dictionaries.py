220a9ad8ac5da46702bbfd88dd5c4a7e18288a2b
gal <gal.rouash@gmail.com>
2020-10-16 18:19:36 +0300

    Update infographics_dictionaries.py

--- anyway/infographics_dictionaries.py
+++ anyway/infographics_dictionaries.py
@@ -1,6 +1,3 @@
 driver_type_hebrew_dict = dict(
     professional_driver="נהג מקצועי", private_vehicle_driver="נהג פרטי", other_driver="לא ידוע"
 )
-head_on_collisions_comparison_dict=dict(
-    head_to_head_collision="התנגשות חזית בחזית", others="אחרות",head_to_head="חזיתיות"
-)

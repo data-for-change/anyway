ca19c6f6e83feaa1e6bae1fc3e001aa5ff1599fc
Carmel Paradis <16carmel@gmail.com>
2021-07-08 11:10:09 +0300

    Added maps key to flask context

--- anyway/flask_app.py
+++ anyway/flask_app.py
@@ -920,6 +920,7 @@ def index(marker=None, message=None):
     context["iteritems"] = dict.items
     context["hide_search"] = True if request.values.get("hide_search") == "true" else False
     context["embedded_reports"] = get_embedded_reports()
+    context['maps_api_key'] = secrets.get('MAPS_API_KEY')
     return render_template("index.html", **context)
 
 

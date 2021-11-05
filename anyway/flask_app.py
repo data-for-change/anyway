350331822000d0a40754c51b8b533b356e2a98fd
Amy Entin <amyentin@gmail.com>
2021-11-05 13:48:46 +0000

    formatting

--- anyway/flask_app.py
+++ anyway/flask_app.py
@@ -1275,15 +1275,12 @@ class RetrieveNewsFlash(Resource):
 def roles_accepted(*roles):
     """Decorator which specifies that a user must have at least one of the
     specified roles. Example::
-
         @app.route('/create_post')
         @roles_accepted('editor', 'author')
         def create_post():
             return 'Create Post'
-
     The current user must have either the `editor` role or `author` role in
     order to view the page.
-
     :param roles: The possible roles.
     """
 

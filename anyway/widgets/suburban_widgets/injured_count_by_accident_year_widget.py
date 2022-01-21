9362e812fa0f2d77fadf69ca02b10ce6b527b44b
bar <58947331+BarVolunteering@users.noreply.github.com>
2022-01-21 15:30:54 +0200

    Issue #2070

--- anyway/widgets/suburban_widgets/injured_count_by_accident_year_widget.py
+++ anyway/widgets/suburban_widgets/injured_count_by_accident_year_widget.py
@@ -1,20 +1,21 @@
+import typing
+from collections import defaultdict
 from typing import Dict
 
 from flask_babel import _
 
-from anyway.request_params import RequestParams
 from anyway.backend_constants import InjurySeverity
 from anyway.infographics_dictionaries import segment_dictionary
+from anyway.models import InvolvedMarkerView
+from anyway.request_params import RequestParams
+from anyway.widgets.suburban_widgets.sub_urban_widget import SubUrbanWidget
+from anyway.widgets.widget import register
 from anyway.widgets.widget_utils import (
     get_accidents_stats,
-    add_empty_keys_to_gen_two_level_dict,
     gen_entity_labels,
     get_injured_filters,
     format_2_level_items,
 )
-from anyway.models import InvolvedMarkerView
-from anyway.widgets.widget import register
-from anyway.widgets.suburban_widgets.sub_urban_widget import SubUrbanWidget
 
 
 @register
@@ -28,6 +29,23 @@ class InjuredCountByAccidentYearWidget(SubUrbanWidget):
             "Fatal, severe and light injured count in the specified years, split by injury severity"
         )
 
+    @staticmethod
+    def sort_by_years_and_severity(
+        data: defaultdict, years_range: typing.Iterable
+    ) -> Dict[int, dict]:
+        res = {}
+        for year in years_range:
+            res[year] = {
+                InjurySeverity.KILLED.value: 0,
+                InjurySeverity.SEVERE_INJURED.value: 0,
+                InjurySeverity.LIGHT_INJURED.value: 0,
+            }
+            severity_in_year = data.get(year, {})
+            for severity, count in severity_in_year.items():
+                res[year][severity] += count
+
+        return res
+
     def generate_items(self) -> None:
         res1 = get_accidents_stats(
             table_obj=InvolvedMarkerView,
@@ -37,17 +55,16 @@ class InjuredCountByAccidentYearWidget(SubUrbanWidget):
             start_time=self.request_params.start_time,
             end_time=self.request_params.end_time,
         )
-        res2 = add_empty_keys_to_gen_two_level_dict(
-            res1,
-            list(range(self.request_params.start_time.year, self.request_params.end_time.year + 1)),
-            InjurySeverity.codes(),
+        res2 = self.sort_by_years_and_severity(
+            res1, range(self.request_params.start_time.year, self.request_params.end_time.year + 1)
         )
         self.items = format_2_level_items(res2, None, InjurySeverity)
 
     @staticmethod
     def localize_items(request_params: RequestParams, items: Dict) -> Dict:
         items["data"]["text"] = {
-            "title": _('Number of injured in accidents, per year, split by severity') +f" - {segment_dictionary[request_params.location_info['road_segment_name']]}",
+            "title": _("Number of injured in accidents, per year, split by severity")
+            + f" - {segment_dictionary[request_params.location_info['road_segment_name']]}",
             "labels_map": gen_entity_labels(InjurySeverity),
         }
         return items

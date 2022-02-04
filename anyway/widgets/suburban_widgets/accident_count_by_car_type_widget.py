0020a18d40c278c6898e6596a35f9e18cb43449a
bar <58947331+BarVolunteering@users.noreply.github.com>
2022-02-04 21:28:55 +0200

    Changes in accident_count_by_car_type #2071: *Change table to VehicleMarkerView *Order the data by segment *Return only top 3

--- anyway/widgets/suburban_widgets/accident_count_by_car_type_widget.py
+++ anyway/widgets/suburban_widgets/accident_count_by_car_type_widget.py
@@ -2,15 +2,19 @@ import logging
 from collections import defaultdict
 from functools import lru_cache
 from typing import Dict
+
 from flask_babel import _
+
+import anyway.widgets.widget_utils as widget_utils
 from anyway.backend_constants import BE_CONST
 from anyway.infographics_dictionaries import segment_dictionary
-from anyway.models import InvolvedMarkerView
-from anyway.vehicle_type import VehicleCategory
-from anyway.widgets.widget import register
-from anyway.widgets.suburban_widgets.sub_urban_widget import SubUrbanWidget
+from anyway.models import VehicleMarkerView
 from anyway.request_params import RequestParams
-import anyway.widgets.widget_utils as widget_utils
+from anyway.vehicle_type import VehicleCategory
+from anyway.widgets.suburban_widgets.sub_urban_widget import SubUrbanWidget
+from anyway.widgets.widget import register
+
+TOP_3 = 2
 
 
 @register
@@ -22,8 +26,10 @@ class AccidentCountByCarTypeWidget(SubUrbanWidget):
         self.rank = 17
 
     def generate_items(self) -> None:
-        self.items = AccidentCountByCarTypeWidget.get_stats_accidents_by_car_type_with_national_data(
-            self.request_params
+        self.items = (
+            AccidentCountByCarTypeWidget.get_stats_accidents_by_car_type_with_national_data(
+                self.request_params
+            )
         )
 
     @staticmethod
@@ -33,10 +39,10 @@ class AccidentCountByCarTypeWidget(SubUrbanWidget):
         out = []
         if involved_by_vehicle_type_data is None:
             involved_by_vehicle_type_data = widget_utils.get_accidents_stats(
-                table_obj=InvolvedMarkerView,
-                filters=widget_utils.get_injured_filters(request_params.location_info),
-                group_by="involve_vehicle_type",
-                count="involve_vehicle_type",
+                table_obj=VehicleMarkerView,
+                filters=request_params.location_info,
+                group_by="vehicle_type",
+                count="vehicle_type",
                 start_time=request_params.start_time,
                 end_time=request_params.end_time,
             )
@@ -46,57 +52,71 @@ class AccidentCountByCarTypeWidget(SubUrbanWidget):
         data_by_segment = AccidentCountByCarTypeWidget.percentage_accidents_by_car_type(
             involved_by_vehicle_type_data
         )
-        national_data = AccidentCountByCarTypeWidget.percentage_accidents_by_car_type_national_data_cache(
-            start_time, end_time
+        national_data = (
+            AccidentCountByCarTypeWidget.percentage_accidents_by_car_type_national_data_cache(
+                start_time, end_time
+            )
         )
 
-        for k, v in national_data.items():  # pylint: disable=W0612
+        for car_type in national_data:
             out.append(
                 {
-                    "car_type": k,
-                    "percentage_segment": data_by_segment[k],
-                    "percentage_country": national_data[k],
+                    "label_key": car_type,
+                    "series": [
+                        {"label_key": "percentage_segment", "value": data_by_segment[car_type]},
+                        {"label_key": "percentage_country", "value": national_data[car_type]},
+                    ],
                 }
             )
         return out
 
     @staticmethod
     def percentage_accidents_by_car_type(involved_by_vehicle_type_data):
-        driver_types = defaultdict(float)
+        vehicle_type_dict = defaultdict(float)
         total_count = 0
         for item in involved_by_vehicle_type_data:
-            vehicle_type, count = item["involve_vehicle_type"], int(item["count"])
+            vehicle_type, count = item["vehicle_type"], int(item["count"])
             total_count += count
             if vehicle_type in VehicleCategory.CAR.get_codes():
-                driver_types[VehicleCategory.CAR.value] += count
+                vehicle_type_dict[VehicleCategory.CAR.value] += count
             elif vehicle_type in VehicleCategory.LARGE.get_codes():
-                driver_types[VehicleCategory.LARGE.value] += count
+                vehicle_type_dict[VehicleCategory.LARGE.value] += count
             elif vehicle_type in VehicleCategory.MOTORCYCLE.get_codes():
-                driver_types[VehicleCategory.MOTORCYCLE.value] += count
+                vehicle_type_dict[VehicleCategory.MOTORCYCLE.value] += count
             elif vehicle_type in VehicleCategory.BICYCLE_AND_SMALL_MOTOR.get_codes():
-                driver_types[VehicleCategory.BICYCLE_AND_SMALL_MOTOR.value] += count
+                vehicle_type_dict[VehicleCategory.BICYCLE_AND_SMALL_MOTOR.value] += count
             else:
-                driver_types[VehicleCategory.OTHER.value] += count
+                vehicle_type_dict[VehicleCategory.OTHER.value] += count
 
-        output = defaultdict(float)
-        for k, v in driver_types.items():  # Calculate percentage
-            output[k] = 100 * v / total_count
+        output = {}
+        # Order the vehicle_type_dict dict by value which is the count of the vehicle_type
+        vehicle_type_dict_ordered = dict(
+            sorted(vehicle_type_dict.items(), key=lambda item: item[1], reverse=True)
+        )
 
-        return output
+        # Calculate percentage of the top 3
+        for i, (key, value) in enumerate(vehicle_type_dict_ordered.items()):
+            output[key] = 100 * value / total_count
+            if i >= TOP_3:
+                break
+
+        # Use defaultdict to return 0 where there is no key, so when there is no data for this type of vehicle the
+        # result will be zero
+        return defaultdict(float, output)
 
     @staticmethod
     @lru_cache(maxsize=64)
     def percentage_accidents_by_car_type_national_data_cache(start_time, end_time):
         involved_by_vehicle_type_data = widget_utils.get_accidents_stats(
-            table_obj=InvolvedMarkerView,
+            table_obj=VehicleMarkerView,
             filters={
                 "road_type": [
                     BE_CONST.ROAD_TYPE_NOT_IN_CITY_NOT_IN_INTERSECTION,
                     BE_CONST.ROAD_TYPE_NOT_IN_CITY_IN_INTERSECTION,
                 ]
             },
-            group_by="involve_vehicle_type",
-            count="involve_vehicle_type",
+            group_by="vehicle_type",
+            count="vehicle_type",
             start_time=start_time,
             end_time=end_time,
         )
@@ -108,11 +128,11 @@ class AccidentCountByCarTypeWidget(SubUrbanWidget):
     def localize_items(request_params: RequestParams, items: Dict) -> Dict:
         for item in items["data"]["items"]:
             try:
-                item["car_type"] = _(VehicleCategory(item["car_type"]).get_english_display_name())
+                item["label_key"] = _(VehicleCategory(item["label_key"]).get_english_display_name())
             except ValueError:
                 logging.exception(f"AccidentCountByCarType.localize_items: item:{item}")
         base_title = _(
-            "comparing vehicle type percentage in accidents in {} relative to national average"
+            "Comparing top 3 vehicle type percentage in accidents in {} relative to national average"
         )
         items["data"]["text"] = {
             "title": base_title.format(

109ec3d9a2c78dd840ff3a7e2092fdf5581caa59
Atalya Alon <atalya.alon@gmail.com>
2022-05-11 22:07:34 +0300

    re-register widget

--- anyway/widgets/suburban_widgets/accident_count_by_car_type_widget.py
+++ anyway/widgets/suburban_widgets/accident_count_by_car_type_widget.py
@@ -16,7 +16,7 @@ from anyway.widgets.suburban_widgets.sub_urban_widget import SubUrbanWidget
 
 MAX_NUMBER_OF_ITEMS = 2
 
-
+@register
 class AccidentCountByCarTypeWidget(SubUrbanWidget):
     name: str = "accident_count_by_car_type"
 

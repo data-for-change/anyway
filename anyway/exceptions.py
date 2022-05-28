4a42b475f09a23780f54deb398e03ed001f3cd4f
Carmel <16carmel@gmail.com>
2022-05-28 19:06:34 +0300

    can be non-abstract

--- anyway/exceptions.py
+++ anyway/exceptions.py
@@ -1,8 +1,7 @@
-from abc import ABC
 from typing import Optional
 
 
-class AnywayError(ABC, Exception):
+class AnywayError(Exception):
     def __init__(self, message: str, status_code: Optional[int] = 500):
         super().__init__(message)
         self.code = status_code

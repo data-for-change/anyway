8c2da97c77fbe0a59a0fd35df86fa28c4bb3005a
BusinessLanguage <rabea.affan@gmail.com>
2022-06-21 20:01:39 +0300

    * Major refactoring of the code * Historic data scraping added (2015-2022)

--- anyway/parsers/scrape_arabic_news.py
+++ anyway/parsers/scrape_arabic_news.py
@@ -1,64 +1,125 @@
-#scrappig using regulare expression:
-#panet-title: "<div class=\"panet-title\"><a href=\"(.*?)\">(.*?)</a><\/div>"gm (CREATES TWO GROUPS ONE FOR LINKS AND SECOND FOR THE TEXT OF THE ARTICLES)
-#panet-abstract: /<div class=\"panet-abstract\"> (.*?)<\/div>/gm
-#panet-time: /<div class=\"panet-time-cont\".<time class=\"panet-time\">(.*?)<\/div>/gm
 
-from dataclasses import dataclass
-import json
 import requests
 import re
+import json
+from datetime import datetime
 
 
-@dataclass
-class PanetArticle:
-    title: str
+class ArticleMetaData:
+    article_pub_date: datetime
     abstract: str
-    time: str
+    title: str
+
+    def __init__(self, _article_pub_date, _abstract, _title):
+        self.article_pub_date = _article_pub_date
+        self.abstract = _abstract
+        self.title = _title
 
 
-def is_panet_accident(title, abstract):
-    keywords = ["حادث طرق", "حوادث طرق", "اصطدام", " دهس", "سائق", "سيارة", "انقلاب", "شاحنة", "مشاة"]
+def scrape(panet_page):
 
-    return any(keyword in title+" "+abstract for keyword in keywords)
+    website_content = requests.get(f"http://www.panet.co.il/category/home/2/{panet_page}")
+
+    """ Historic article titles: """
+
+    regex1 = r"<div class=\"panet-title\">(.*?)<\/div>"
+    matches1 = re.finditer(regex1, website_content.text, re.MULTILINE)
+
+    with open(f"titles_{panet_page}.txt", "w", encoding="utf-8") as f1:
+
+        for matchNum, match in enumerate(matches1, start=1):
+
+            for groupNum in range(0, len(match.groups())):
+                groupNum = groupNum + 1
+                f1.write(match.group(groupNum))
+                f1.write('\n')
+
+    """ Historic article abstracts: """
+
+    regex2 = "<div class=\"panet-abstract\">(.*?)<(?:.*?)</div>"
+    matches2 = re.finditer(regex2, website_content.text, re.MULTILINE)
+
+    with open(f"abstracts_{panet_page}.txt", "w", encoding="utf-8") as f2:
+        for matchNum, match in enumerate(matches2, start=1):
+            f2.write(match.group(1))
+            f2.write('\n')
+
+    """ Historic article pub time: """
+
+    regex3 = r"<div class=\"panet-time-cont\".<time class=\"panet-time\">(.*?)<\/div>"
+    matches3 = re.finditer(regex3, website_content.text, re.MULTILINE)
+
+    with open(f"articles-pub-time_{panet_page}.txt", 'w', encoding='utf-8') as f3:
+        for matchNum, match in enumerate(matches3, start=1):
+
+            for groupNum in range(0, len(match.groups())):
+                groupNum = groupNum + 1
+                f3.write(match.group(groupNum))
+                f3.write('\n')
+
+    """  Code to scraping ABSTRACTS in a specified page range """
 
 
-def scrape():
-    panet_dict = dict()
+def scrape_abstracts():
 
+    for page in range(1, 5):
+        content = requests.get(f"http://www.panet.co.il/category/home/2/{page}/")
+        content_txt = content.text
+        with open(f"PanetPageText{page}.txt", "w", encoding='utf-8') as file_txt:
+            regex2 = r"<div class=\"panet-abstract\">(.*?)<(.*?)</div>"
+            matches2 = re.finditer(regex2, content_txt, re.MULTILINE)
+            for matchNum, match in enumerate(matches2, start=1):
+
+                for groupNum in range(0, len(match.groups())):
+                    file_txt.write(match.group(1))
+                    file_txt.write('\n')
+
+
+def generate_article_dict_and_classify(panet_page):
+
+    key_list = [line.strip('\n') for line in open(f"articles-pub-time_{panet_page}.txt", 'r', encoding='utf-8')]
+    value_list = [line.strip('\n') for line in open(f"abstracts_{panet_page}.txt", 'r', encoding='utf-8')]
+    # print(len(key_list))
+    # print(len(value_list))
+    assert len(key_list) == len(value_list)
+    article_dict = dict(zip(key_list, value_list))
+    with open(f"article_dict_{panet_page}.txt", "w", encoding='utf-8') as f4:
+        f4.write(json.dumps(article_dict))
+
+    key_keywords = ["حادث طرق", "حوادث طرق", "اصطدام", " دهس", "سائق", "سيارة", "انقلاب", "شاحنة", "مشاة"]
+    counter = 0
+    for line in value_list:
+        if any([keyword in line for keyword in key_keywords]):
+            counter += 1
+            with open(f"Car_Accident_News_Flash_Number_{counter}.txt", "w", encoding='utf-8') as CarAccidentNews:
+                CarAccidentNews.write(line.strip())
+                CarAccidentNews.write('\n')
+
+
+def unified_scrape():
+
+    all_articles_dict = {}
     website_content = requests.get("http://www.panet.co.il/category/home/2")
-
-    #panet-titles:
-    regex = r"<div class=\"panet-title\"><a href=\"(.*?)\">(.*?)</a><\/div><div class=\"panet-abstract\">(.*?)</div></div></div><div class=\"panet-time-cont\".<time class=\"panet-time\">(.*?)<\/div>"
-    matches1 = re.finditer(regex, website_content.text, re.MULTILINE)
-
+    regex1 = r"<div class=\"panet-title\"><a href=\"/article/(.*?)\">(.*?)<\/a><\/div>"
+    matches1 = re.finditer(regex1, website_content.text, re.MULTILINE)
     for matchNum, match in enumerate(matches1, start=1):
-        url = match.group(1)
-        title = match.group(2)
-        abstract = match.group(3)
-        time = match.group(4)
-        if is_panet_accident(title, abstract):
-            article = PanetArticle(title=title, abstract=abstract, time=time)
-            panet_dict[url] = article
+        article_number = int(match.group(1))
+        article_title = match.group(2)
+        all_articles_dict[article_number] = ArticleMetaData(
+            _article_pub_date=datetime.now(),
+            _title=article_title.strip(),
+            _abstract=""
+        )
 
-    f = open("panet/panet_accidents.tsv", "w")
-    for url in panet_dict.keys():
-        article = panet_dict[url]
-        f.write(f"{url}\t{article.title}\t{article.time}\t{article.abstract},\n")
-    f.close()
+    with open("articles.tsv", "w") as f:
+        for tsv in all_articles_dict.keys():
+            val = all_articles_dict[tsv]
+            f.write(f"{tsv}\t{val.title}\t{val.article_pub_date}\n")
 
 
 if __name__ == '__main__':
-    scrape()
-
-    """
-    for page in range(1, 10):
-        content = requests.get(f"http://www.panet.co.il/category/home/2/{page}/")
-        contenttxt = content.text
-        file_txt = open(f"/Users/rabea/PycharmProjects/Efraim/PanetPage{page}.txt", "w")
-        file_txt.write(contenttxt)
-        file_txt.close()
-
-        # file_yaml = open(f"/Users/rabea/PycharmProjects/Efraim/PanetPage{page}.yaml", "w")
-        # file_yaml.write(contenttxt)
-        # file_yaml.close()
-    """
+    for panet_page in range(0, 301):
+        scrape(panet_page)
+        generate_article_dict_and_classify(panet_page)
+        # scrape_abstracts()
+        # unified_scrape()

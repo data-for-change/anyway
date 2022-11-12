cd505b396407d6877b3240c204c4c63708cf491e
bar <58947331+BarVolunteering@users.noreply.github.com>
2022-11-12 12:48:02 +0200

    Change sleep time

--- wait_for_postgres.sh
+++ wait_for_postgres.sh
@@ -2,6 +2,6 @@ RETRIES=30
 export PGPASSWORD=anyway
 until psql -h 0.0.0.0 -U anyway -d anyway -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
   echo "Waiting for postgres server, $((RETRIES--)) remaining attempts..."
-  sleep 5
+  sleep 1
 done
 sleep 10

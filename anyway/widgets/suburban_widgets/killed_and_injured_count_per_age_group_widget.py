fb0814b55f8a6d8e0c5e34211c52d7e6fbea8f8e
Adir Bruchim <adir.bruchim@catonetworks.com>
2023-01-01 08:15:21 +0200

    Removed the "In" from the subtitle.

--- anyway/widgets/suburban_widgets/killed_and_injured_count_per_age_group_widget.py
+++ anyway/widgets/suburban_widgets/killed_and_injured_count_per_age_group_widget.py
@@ -39,6 +39,6 @@ class KilledInjuredCountPerAgeGroupWidget(SubUrbanWidget):
     def localize_items(request_params: RequestParams, items: Dict) -> Dict:
         items["data"]["text"] = {
             "title": _("Injury per age group"),
-            "subtitle": _("In") + " " + request_params.location_info["road_segment_name"],
+            "subtitle": request_params.location_info["road_segment_name"],
         }
         return items

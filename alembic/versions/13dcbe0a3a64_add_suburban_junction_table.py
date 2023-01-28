c13613c05a2a4354ae59b257f52df352a834face
ziv <ziv_harpaz@yahoo.com>
2023-01-28 16:37:10 +0200

    Add suburban junction table. Load data from markers table. Renamed anyway/scripts/wikipedia_scraper_road_images.py to bypass black issue.

---
+++ alembic/versions/13dcbe0a3a64_add_suburban_junction_table.py
@@ -0,0 +1,37 @@
+"""add suburban junction table
+
+Revision ID: 13dcbe0a3a64
+Revises: ccc480c5d0a2
+Create Date: 2023-01-16 12:39:03.026479
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '13dcbe0a3a64'
+down_revision = 'ccc480c5d0a2'
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+
+def upgrade():
+    op.create_table(
+        'suburban_junction',
+        sa.Column('non_urban_intersection', sa.Integer(), primary_key=True, nullable=False, default=None),
+        sa.Column('non_urban_intersection_hebrew', sa.VARCHAR(length=100)),
+        sa.Column('roads', postgresql.ARRAY(sa.Integer(), dimensions=1), nullable=False),
+    )
+    op.create_index('suburban_junction_non_urban_intersection_idx',
+                    'suburban_junction',
+                    ['non_urban_intersection'], unique=True
+                    )
+
+
+def downgrade():
+    op.drop_index('suburban_junction_non_urban_intersection_idx',
+                  table_name='suburban_junction'
+                  )
+    op.drop_table('suburban_junction')

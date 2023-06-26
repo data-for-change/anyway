915568e61ca8a1ab1cfdde48769b7f7a7b8aa539
Tamar Kalir <tkalir@gmail.com>
2023-06-26 11:49:22 +0300

    refactor telegram's publish_notification

--- anyway/telegram_accident_notifications.py
+++ anyway/telegram_accident_notifications.py
@@ -4,8 +4,11 @@ from anyway import secrets
 import telebot
 import boto3
 import time
+import requests
 
+ANYWAY_BASE_API_URL = "https://www.anyway.co.il/api"
 INFOGRAPHICS_S3_BUCKET = "dfc-anyway-infographics-images"
+
 TELEGRAM_CHANNEL_CHAT_ID = -1001666083560
 TELEGRAM_LINKED_GROUP_CHAT_ID = -1001954877540
 
@@ -14,7 +17,7 @@ def send_initial_message_in_channel(bot, text):
     return bot.send_message(TELEGRAM_CHANNEL_CHAT_ID, text)
 
 
-def fetch_message_id_for_thread_starting_message_in_group(bot, thread_starting_message_in_channel):
+def fetch_message_id_for_initial_message_in_discussion_group(bot, thread_starting_message_in_channel):
     tries = 3
     for _ in range(tries):
         time.sleep(10)
@@ -27,31 +30,42 @@ def fetch_message_id_for_thread_starting_message_in_group(bot, thread_starting_m
     return None
 
 
-def publish_notification(newsflash_id):
-    import requests
-
-    anyway_base_api_url = "https://www.anyway.co.il/api"
-    newsflash_json = requests.get(f"{anyway_base_api_url}/news-flash/{newsflash_id}").json()
+def create_accident_text(newsflash_id):
+    newsflash_json = requests.get(f"{ANYWAY_BASE_API_URL}/news-flash/{newsflash_id}").json()
     newsflash_title = newsflash_json["title"]
     newsflash_description = newsflash_json["description"]
-    widgets_url = f"{anyway_base_api_url}/infographics-data?lang=he&news_flash_id={newsflash_id}&years_ago=5"
-    widgets_json = requests.get(widgets_url).json()
-    transcript_by_widget_name = {widget["name"]: widget["data"]["text"]["transcription"]
-                                 for widget in widgets_json["widgets"]
-                                 if "transcription" in widget["data"]["text"]}
-    bot = telebot.TeleBot(secrets.get("TELEGRAM_BOT_TOKEN"))
+    return f"{newsflash_title}\n\n{newsflash_description}"
 
-    title_and_description = f"{newsflash_title}\n\n{newsflash_description}"
-    thread_starting_message_in_channel = send_initial_message_in_channel(bot, title_and_description)
-    message_id_in_group = fetch_message_id_for_thread_starting_message_in_group(bot, thread_starting_message_in_channel)
+
+def fetch_transcription_by_widget_name(newsflash_id):
+    widgets_url = f"{ANYWAY_BASE_API_URL}/infographics-data?lang=he&news_flash_id={newsflash_id}&years_ago=5"
+    widgets_json = requests.get(widgets_url).json()
+    transcription_by_widget_name = {widget["name"]: widget["data"]["text"]["transcription"]
+                                    for widget in widgets_json["widgets"]
+                                    if "transcription" in widget["data"]["text"]}
+    return transcription_by_widget_name
+
+
+def publish_notification(newsflash_id):
+    #fetch data for send
+    accident_text = create_accident_text(newsflash_id)
+    transcription_by_widget_name = fetch_transcription_by_widget_name(newsflash_id)
+    urls_by_infographic_name = create_public_urls_for_infographics_images(str(newsflash_id))
+
+    bot = telebot.TeleBot(secrets.get("TELEGRAM_BOT_TOKEN"))
+    initial_message_in_channel = send_initial_message_in_channel(bot, accident_text)
+    #every message in the channel is automatically forwarded to the linked discussion group.
+    #to create a comment on the channel message, we need to send a reply to the
+    #forwareded message in the discussion group.
+    message_id_in_group = \
+        fetch_message_id_for_initial_message_in_discussion_group(bot, initial_message_in_channel)
 
     if message_id_in_group:
-        urls_by_infographic_name = create_public_urls_for_infographics_images(str(newsflash_id))
         for infographic_name, url in urls_by_infographic_name.items():
-            text = transcript_by_widget_name[infographic_name] \
-                if infographic_name in transcript_by_widget_name else None
+            text = transcription_by_widget_name[infographic_name] \
+                if infographic_name in transcription_by_widget_name else None
             bot.send_photo(TELEGRAM_LINKED_GROUP_CHAT_ID, url, reply_to_message_id=message_id_in_group, caption=text)
-
+    logging.info("notification send done")
 
 def extract_infographic_name_from_s3_object(s3_object_name):
     left = s3_object_name.rindex("/")

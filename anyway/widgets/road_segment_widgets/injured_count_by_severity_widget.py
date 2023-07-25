6a0ec3c0b99bc827f59a4891e90d51e8cc296ea0
Atalya Alon <atalya.alon@gmail.com>
2023-07-25 17:56:16 +0300

    bug fix and black

--- anyway/widgets/road_segment_widgets/injured_count_by_severity_widget.py
+++ anyway/widgets/road_segment_widgets/injured_count_by_severity_widget.py
@@ -68,49 +68,50 @@ class InjuredCountBySeverityWidget(RoadSegmentWidget):
         items["total_injured_count"] = total_injured_count
         return items
 
-
     @staticmethod
     def get_transcription(request_params: RequestParams, items: Dict):
         total_injured_count = items.get("total_injured_count")
         if total_injured_count == 0:
-            return ''
+            return ""
         severity_light_count = items.get("light_injured_count")
         if severity_light_count == 0:
-            severity_light_count_text = ''
+            severity_light_count_text = ""
         elif severity_light_count == 1:
             severity_light_count_text = _("one light injured")
         else:
-            severity_light_count_text = f'{severity_light_count} ' +  _("light injured plural")
+            severity_light_count_text = f"{severity_light_count} " + _("light injured plural")
         severity_severe_count = items.get("severe_injured_count")
         if severity_severe_count == 0:
-            severity_severe_count_text = ''
+            severity_severe_count_text = ""
         elif severity_severe_count == 1:
             severity_severe_count_text = _("one severe injured")
         else:
-            severity_severe_count_text = f'{severity_severe_count} '+  _("severe injured plural")
+            severity_severe_count_text = f"{severity_severe_count} " + _("severe injured plural")
         killed_count = items.get("killed_count")
         if killed_count == 0:
-            killed_count = ''
+            killed_count_text = ""
         elif killed_count == 1:
             killed_count_text = _("one killed")
         else:
-            killed_count_text = f'{killed_count} ' +  _("killed plural")
+            killed_count_text = f"{killed_count} " + _("killed plural")
         if request_params.resolution == BE_CONST.ResolutionCategories.STREET:
             text = "{in_yishuv_keyword} {yishuv_name} {in_street_keyword} {street_name} ".format(
                 in_yishuv_keyword=_("in yishuv"),
-                yishuv_name=_(request_params.location_info.get('yishuv_name')),
+                yishuv_name=_(request_params.location_info.get("yishuv_name")),
                 in_street_keyword=_("in street"),
-                street_name=_(request_params.location_info.get('street1_hebrew')),
+                street_name=_(request_params.location_info.get("street1_hebrew")),
             )
         elif request_params.resolution == BE_CONST.ResolutionCategories.SUBURBAN_ROAD:
             text = "{in_road_keyword} {road_num} {in_segment_keyword} {segment_name} ".format(
                 in_road_keyword=_("in road"),
-                road_num=request_params.location_info.get('road1'),
+                road_num=request_params.location_info.get("road1"),
                 in_segment_keyword=_("in segment"),
-                segment_name=_(request_params.location_info.get('road_segment_name')),
+                segment_name=_(request_params.location_info.get("road_segment_name")),
             )
         else:
-            raise Exception(f"cannot convert to hebrew for resolution : {request_params.resolution.get('resolution')}")
+            raise Exception(
+                f"cannot convert to hebrew for resolution : {request_params.resolution.get('resolution')}"
+            )
         text += "{between_years_keyword} {start_year} - {end_year}, {injured_killed_keyword} {injured_num} {people_phrase}, {out_of_them_keywoard} ".format(
             between_years_keyword=_("between the years"),
             start_year=request_params.start_time.year,
@@ -120,19 +121,21 @@ class InjuredCountBySeverityWidget(RoadSegmentWidget):
             people_phrase=_("people from car accidents"),
             out_of_them_keywoard=_("out of them"),
         )
-        text += join_strings([killed_count_text, severity_severe_count_text, severity_light_count_text],
-                                sep_a=" ,",
-                                sep_b=_(" and "))
+        text += join_strings(
+            [killed_count_text, severity_severe_count_text, severity_light_count_text],
+            sep_a=" ,",
+            sep_b=_(" and "),
+        )
         return text
-
 
     @staticmethod
     def localize_items(request_params: RequestParams, items: Dict) -> Dict:
         items["data"]["text"] = {
             "title": _("Number of Injuries in accidents by severity"),
-            "subtitle": _(request_params.location_info['road_segment_name']),
-            "transcription": InjuredCountBySeverityWidget.get_transcription(request_params=request_params,
-                                                                            items=items["data"]["items"])
+            "subtitle": _(request_params.location_info["road_segment_name"]),
+            "transcription": InjuredCountBySeverityWidget.get_transcription(
+                request_params=request_params, items=items["data"]["items"]
+            ),
         }
         return items
 

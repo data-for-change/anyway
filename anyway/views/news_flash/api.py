c6b93532052d0168d63c882b232b722b6cd75cd6
Atalya Alon <atalya.alon@gmail.com>
2024-05-06 16:48:58 +0300

    remove unused APIs and update news flash model

--- anyway/views/news_flash/api.py
+++ anyway/views/news_flash/api.py
@@ -69,41 +69,6 @@ class NewsFlashQuery(BaseModel):
 
 
 def news_flash():
-    news_flash_id = request.values.get("id")
-
-    if news_flash_id is not None:
-        query = db.session.query(NewsFlash)
-        news_flash_obj = query.filter(NewsFlash.id == news_flash_id).first()
-        if news_flash_obj is not None:
-            if is_news_flash_resolution_supported(news_flash_obj):
-                return Response(
-                    json.dumps(news_flash_obj.serialize(), default=str), mimetype="application/json"
-                )
-            else:
-                return Response("News flash location not supported", 406)
-        return Response(status=404)
-
-    query = gen_news_flash_query(
-        db.session,
-        source=request.values.get("source"),
-        start_date=request.values.get("start_date"),
-        end_date=request.values.get("end_date"),
-        interurban_only=request.values.get("interurban_only"),
-        road_number=request.values.get("road_number"),
-        road_segment=request.values.get("road_segment_only"),
-        last_minutes=request.values.get("last_minutes"),
-        offset=request.values.get("offset", DEFAULT_OFFSET_REQ_PARAMETER),
-        limit=request.values.get("limit", DEFAULT_LIMIT_REQ_PARAMETER),
-    )
-    news_flashes = query.all()
-
-    news_flashes_jsons = [n.serialize() for n in news_flashes]
-    for news_flash in news_flashes_jsons:
-        set_display_source(news_flash)
-    return Response(json.dumps(news_flashes_jsons, default=str), mimetype="application/json")
-
-
-def news_flash_v2():
     requested_query_params = normalize_query(request.args)
     try:
         validated_query_params = NewsFlashQuery(**requested_query_params).dict(exclude_none=True)
@@ -113,7 +78,7 @@ def news_flash_v2():
     if "id" in validated_query_params:
         return get_news_flash_by_id(validated_query_params["id"])
 
-    query = gen_news_flash_query_v2(db.session, validated_query_params)
+    query = gen_news_flash_query(db.session, validated_query_params)
     news_flashes = query.all()
 
     news_flashes_jsons = [n.serialize() for n in news_flashes]
@@ -122,94 +87,7 @@ def news_flash_v2():
     return Response(json.dumps(news_flashes_jsons, default=str), mimetype="application/json")
 
 
-def news_flash_new(args: dict) -> List[dict]:
-    news_flash_id = args["id"]
-
-    if news_flash_id is not None:
-        return single_news_flash(news_flash_id)
-
-    query = gen_news_flash_query(
-        db.session,
-        source=args.get("source"),
-        start_date=args.get("start_date"),
-        end_date=args.get("end_date"),
-        interurban_only=args.get("interurban_only"),
-        road_number=args.get("road_number"),
-        road_segment=args.get("road_segment_only"),
-        offset=args.get("offset"),
-        limit=args.get("limit"),
-        last_minutes=args.get("last_minutes"),
-    )
-    news_flashes = query.all()
-
-    news_flashes_jsons = [n.serialize() for n in news_flashes]
-    for news_flash in news_flashes_jsons:
-        set_display_source(news_flash)
-    return news_flashes_jsons
-
-
-def gen_news_flash_query(
-    session,
-    source=None,
-    start_date=None,
-    end_date=None,
-    interurban_only=None,
-    road_number=None,
-    road_segment=None,
-    offset=None,
-    limit=None,
-    last_minutes=None
-):
-    query = session.query(NewsFlash)
-    # get all possible sources
-    sources = [
-        str(source_name[0]) for source_name in db.session.query(NewsFlash.source).distinct().all()
-    ]
-    if source:
-        if source not in sources:
-            return Response(
-                '{"message": "Requested source does not exist"}',
-                status=404,
-                mimetype="application/json",
-            )
-        else:
-            query = query.filter(NewsFlash.source == source)
-
-    if start_date and end_date:
-        query = filter_by_timeframe(end_date, query, start_date)
-    # when only one of the dates is sent
-    elif start_date or end_date:
-        return Response(
-            '{"message": "Must send both start_date and end_date"}',
-            status=404,
-            mimetype="application/json",
-        )
-    supported_resolutions = set([x.value for x in BE_CONST.SUPPORTED_RESOLUTIONS])
-    query = query.filter(NewsFlash.resolution.in_(supported_resolutions))
-    if interurban_only == "true" or interurban_only == "True":
-        query = query.filter(NewsFlash.resolution.in_(["כביש בינעירוני"]))
-    if road_number:
-        query = query.filter(NewsFlash.road1 == road_number)
-    if road_segment == "true":
-        query = query.filter(not_(NewsFlash.road_segment_id == None))
-    if last_minutes:
-        last_timestamp = datetime.datetime.now() - datetime.timedelta(minutes=last_minutes)
-        query = query.filter(NewsFlash.date >= last_timestamp)
-    query = query.filter(
-        and_(
-            NewsFlash.accident == True,
-            not_(and_(NewsFlash.lat == 0, NewsFlash.lon == 0)),
-            not_(and_(NewsFlash.lat == None, NewsFlash.lon == None)),
-        )
-    ).order_by(NewsFlash.date.desc())
-
-    query = query.offset(offset)
-    query = query.limit(limit)
-
-    return query
-
-
-def gen_news_flash_query_v2(session, valid_params: dict):
+def gen_news_flash_query(session, valid_params: dict):
     query = session.query(NewsFlash)
     for param, value in valid_params.items():
         if param == "road_number":
@@ -233,8 +111,10 @@ def gen_news_flash_query_v2(session, valid_params: dict):
             not_(and_(NewsFlash.lat == None, NewsFlash.lon == None)),
         )
     ).order_by(NewsFlash.date.desc())
-    query = query.offset(valid_params["offset"])
-    query = query.limit(valid_params["limit"])
+    if "offset" in valid_params:
+        query = query.offset(valid_params["offset"])
+    if "limit" in valid_params:
+        query = query.limit(valid_params["limit"])
     return query
 
 

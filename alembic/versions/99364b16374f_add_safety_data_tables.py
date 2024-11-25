143a57ba6a4297ca9d1ef7ee80e1bdd2fc756c4d
ziv <ziv_harpaz@yahoo.com>
2024-11-25 19:00:32 +0200

    Adding safety-data tables, /involved, /involved/groupby and /city queries. Add population field to cbs_cities from data.gov Add id_osm, lat, lon to cbs_cities from OSM Add process safety-data-tables command

---
+++ alembic/versions/99364b16374f_add_safety_data_tables.py
@@ -0,0 +1,102 @@
+"""Add safety-data tables
+
+Revision ID: 99364b16374f
+Revises: e962054e4422
+Create Date: 2024-11-15 11:03:26.435210
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '99364b16374f'
+down_revision = 'e962054e4422'
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+sd_involved_table = "safety_data_involved"
+sd_accident_table = "safety_data_accident"
+cities_table = "cbs_cities"
+
+
+def downgrade():
+    op.drop_table(sd_involved_table) # pylint: disable=no-member
+    op.drop_table(sd_accident_table) # pylint: disable=no-member
+    op.drop_column(cities_table, 'population') # pylint: disable=no-member
+    op.drop_column(cities_table, 'id_osm') # pylint: disable=no-member
+    op.drop_column(cities_table, 'lat') # pylint: disable=no-member
+    op.drop_column(cities_table, 'lon') # pylint: disable=no-member
+
+
+def upgrade():
+    create_safety_data_accident_table()
+    create_safety_data_involved_table()
+    op.add_column(cities_table, sa.Column('population', sa.Integer(), nullable=True)) # pylint: disable=no-member
+    op.add_column(cities_table, sa.Column('id_osm', sa.BigInteger(), nullable=True)) # pylint: disable=no-member
+    op.add_column(cities_table, sa.Column('lat', sa.Float(), nullable=True)) # pylint: disable=no-member
+    op.add_column(cities_table, sa.Column('lon', sa.Float(), nullable=True)) # pylint: disable=no-member
+
+def create_safety_data_accident_table():
+    op.create_table( # pylint: disable=no-member
+        sd_accident_table,
+        sa.Column('accident_id', sa.BigInteger(), primary_key=True, autoincrement=False, nullable=False),
+        sa.Column('accident_year', sa.Integer(), primary_key=True, autoincrement=False, nullable=False),
+        sa.Column('provider_code', sa.Integer(), primary_key=True, autoincrement=False, nullable=False),
+        sa.Column('accident_month', sa.Integer()),
+        sa.Column('accident_timestamp', sa.TIMESTAMP()),
+        sa.Column('accident_type', sa.Integer(), nullable=True),
+        sa.Column('accident_yishuv_symbol', sa.Integer(), nullable=True),
+        sa.Column('day_in_week', sa.Integer(), nullable=True),
+        sa.Column('day_night', sa.Integer(), nullable=True),
+        sa.Column('location_accuracy', sa.Integer(), nullable=True),
+        sa.Column('multi_lane', sa.Integer(), nullable=True),
+        sa.Column('one_lane', sa.Integer(), nullable=True),
+        sa.Column('road1', sa.Integer(), nullable=True),
+        sa.Column('road2', sa.Integer(), nullable=True),
+        sa.Column('road_segment_number', sa.Integer(), nullable=True),
+        sa.Column('road_type', sa.Integer(), nullable=True),
+        sa.Column('road_width', sa.Integer(), nullable=True),
+        sa.Column('speed_limit', sa.Integer(), nullable=True),
+        sa.Column('street1', sa.Integer(), nullable=True),
+        sa.Column('street2', sa.Integer(), nullable=True),
+        sa.Column('vehicles', sa.Integer(), nullable=True),
+        sa.Column('latitude', sa.Float(), nullable=True),
+        sa.Column('longitude', sa.Float(), nullable=True),
+    )
+    for field in ['accident_year', 'accident_month', 'accident_timestamp',
+                  'accident_type', 'accident_yishuv_symbol',
+                  'day_night', 'multi_lane', 'one_lane',
+                  'road1', 'road2', 'road_segment_number', 'road_type', 'road_width',
+                  'speed_limit',
+                  'street1', 'street2',
+                  'latitude', 'longitude',
+                  ]:
+         # pylint: disable=no-member
+        op.create_index(op.f(f'ix_{sd_accident_table}_{field}'), sd_accident_table, [field], unique=False)
+
+def create_safety_data_involved_table():
+    op.create_table( # pylint: disable=no-member
+        sd_involved_table,
+        sa.Column('_id', sa.Integer(), primary_key=True, autoincrement=False, nullable=False),
+        sa.Column('accident_id', sa.BigInteger(), nullable=False),
+        sa.Column('accident_year', sa.Integer(), nullable=False),
+        sa.Column('provider_code', sa.Integer(), nullable=False),
+        sa.Column('age_group', sa.Integer(), nullable=True),
+        sa.Column('injured_type', sa.Integer(), nullable=True),
+        sa.Column('injury_severity', sa.Integer(), nullable=True),
+        sa.Column('population_type', sa.Integer(), nullable=True),
+        sa.Column('sex', sa.Integer(), nullable=True),
+        sa.Column('vehicle_type', sa.Integer(), nullable=True),
+    )
+    op.create_foreign_key(f'{sd_involved_table}_accident_id_fkey', # pylint: disable=no-member
+                          sd_involved_table, sd_accident_table,
+                          ['accident_id', 'provider_code', 'accident_year'],
+                          ['accident_id', 'provider_code', 'accident_year'],
+                          ondelete='CASCADE')
+    op.create_index(op.f(f'ix_{sd_involved_table}_inv_acc'), sd_involved_table, # pylint: disable=no-member
+                    ['accident_id', 'accident_year', 'provider_code'], unique=False)
+    for field in ['injury_severity', 'injured_type',
+                  'age_group', 'sex', 'population_type', 'vehicle_type']:
+         # pylint: disable=no-member
+        op.create_index(op.f(f'ix_{sd_involved_table}_{field}'), sd_involved_table, [field], unique=False)
---
+++ tests/test_involved_query.py
@@ -0,0 +1,33 @@
+import unittest
+from anyway.views.safety_data.involved_query import InvolvedQuery
+from anyway.views.safety_data.involved_query_gb import InvolvedQuery_GB
+
+class TestInvolvedQuery(unittest.TestCase):
+    def test_vehicle_type_bit_2_heb(self):
+        f = InvolvedQuery.vehicle_type_bit_2_heb
+        self.assertEqual(f(1 << 1), "רכב נוסעים פרטי")
+        self.assertEqual(f(0), "")
+        self.assertEqual(f(1 << 3), "טנדר")
+        self.assertEqual(f(1 << 20), "")
+        self.assertEqual(f(1 << 17), "אחר ולא ידוע")
+        self.assertEqual(f(1 << 0), "")
+        self.assertEqual(f(1 << 26), "")
+        self.assertEqual(f((1 << 1) | (1 << 2)), "רכב נוסעים פרטי, טרנזיט")
+        self.assertEqual(f((1 << 21) | (1 << 25)), "קורקינט חשמלי, משאית")
+
+    def test_dictify_double_group_by(self):
+        data = [
+            ("2021", "Male", 10),
+            ("2021", "Female", 5),
+            ("2022", "Male", 7),
+            ("2022", "Female", 3),
+        ]
+        expected = [
+            {"_id": "2021", "count": [{"grp2": "Male", "count": 10}, {"grp2": "Female", "count": 5}]},
+            {"_id": "2022", "count": [{"grp2": "Male", "count": 7}, {"grp2": "Female", "count": 3}]},
+        ]
+        actual = InvolvedQuery_GB.dictify_double_group_by(data)
+        self.assertEqual(actual, expected)
+
+if __name__ == '__main__':
+    unittest.main()

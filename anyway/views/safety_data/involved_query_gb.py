738051aa1561d5f60383c7bfac3f7a5b9fc73487
ziv <ziv_harpaz@yahoo.com>
2025-01-26 14:11:17 +0200

    WIP, single, double groupby, without filter

--- anyway/views/safety_data/involved_query_gb.py
+++ anyway/views/safety_data/involved_query_gb.py
@@ -1,4 +1,4 @@
-from typing import List, Dict, Optional
+from typing import List, Dict, Optional, Tuple, Any
 import math
 import pandas as pd
 from sqlalchemy.orm import aliased
@@ -42,23 +42,67 @@ class InvolvedQuery_GB(InvolvedQuery):
     def get_data(self) -> List[Dict[str, Optional[str]]]:
         vals = self.get_params()
         gb = vals.pop(GB, None)
-        if gb is None:
-            msg = f"'gb' missing: params: {vals}"
+        if gb is None or len(gb) != 1:
+            msg = f"'gb' missing or invalid: params: {vals}"
             logging.error(msg)
             raise ValueError(msg)
+        gb = gb[0]
         gb2 = vals.pop(GB2, None)
+        if gb2 is not None:
+            if len(gb2) != 1:
+                msg = f"'gb2' should contain a single value: params: {vals}"
+                logging.error(msg)
+                raise ValueError(msg)
+            gb2 = gb2[0]
         query = self.get_base_query()
         query = ParamFilterExp.add_params_filter(query, vals)
-        query = self.add_gb_filter(query, gb)
+        query = self.add_gb_filter(query, gb, gb2)
         # pylint: disable=no-member
         data = query.all()
-        if gb2 is not None:
-            data = wd.retro_dictify(data)
+        if gb2 is None:
+            res = [{"_id": x[0], "count": x[1]} for x in data]
+        else:
+            res = self.dictify_double_group_by(data)
         # [self.add_text(d) for d in data]
-        return data
+        return res
 
-    def add_gb_filter(self, query: Query, gb: str) -> Query:
+    def add_gb_filter(self, query: Query, gb: str, gb2: Optional[str]) -> Query:
+        c1 = GBFilt2Col.get_col(gb)
+        if gb2 is None:
+            return (query
+                    .group_by(c1)
+                    # pylint: disable=no-member
+                    .with_entities(c1.label(gb), db.func.count(SDInvolved._id).label("count"))
+            )
+        c2 = GBFilt2Col.get_col(gb2)
         return (query
-                .group_by(SDAccident.accident_year, Sex.sex_hebrew)
-                .with_entities(SDAccident.accident_year, Sex.sex_hebrew, db.func.count(SDInvolved._id).label("count"))
+                .group_by(c1, c2)
+                # pylint: disable=no-member
+                .with_entities(c1, c2, db.func.count(SDInvolved._id).label("count"))
         )
+
+    @staticmethod
+    def dictify_double_group_by(data: List[Tuple[Any, Any, Any]]) -> List[Dict[str, Any]]:
+        r = wd.retro_dictify(data)
+        res = []
+        for k, v in r.items():
+            res.append({"_id": k, "count": [{"grp2": k1, "count": v1} for k1, v1 in v.items()]})
+        return res
+
+class GBFilt2Col:
+    PFE = {
+        "year": {
+            "col": SDAccident.accident_year,
+        },
+        "sex": {
+            "col": Sex.sex_hebrew,
+        },
+    }
+
+    @staticmethod
+    def get_col(filt: str) -> Column:
+        if filt not in GBFilt2Col.PFE:
+            msg = f"filter not recognized as group by: {filt}"
+            logging.error(msg)
+            raise ValueError(msg)
+        return GBFilt2Col.PFE[filt]["col"]
--- tests/test_involved_query.py
+++ tests/test_involved_query.py
@@ -1,5 +1,8 @@
 import unittest
+import sys
+import os
 from anyway.views.safety_data.involved_query import InvolvedQuery
+from anyway.views.safety_data.involved_query_gb import InvolvedQuery_GB
 
 class TestInvolvedQuery(unittest.TestCase):
     def test_vehicle_type_bit_2_heb(self):
@@ -13,6 +16,20 @@ class TestInvolvedQuery(unittest.TestCase):
         self.assertEqual(f(1 << 26), "")
         self.assertEqual(f((1 << 1) | (1 << 2)), "רכב נוסעים פרטי, טרנזיט")
         self.assertEqual(f((1 << 21) | (1 << 25)), "קורקינט חשמלי, משאית")
+
+    def test_dictify_double_group_by(self):
+        data = [
+            ("2021", "Male", 10),
+            ("2021", "Female", 5),
+            ("2022", "Male", 7),
+            ("2022", "Female", 3),
+        ]
+        expected = [
+            {"_id": "2021", "count": [{"grp2": "Male", "count": 10}, {"grp2": "Female", "count": 5}]},
+            {"_id": "2022", "count": [{"grp2": "Male", "count": 7}, {"grp2": "Female", "count": 3}]},
+        ]
+        actual = InvolvedQuery_GB.dictify_double_group_by(data)
+        self.assertEqual(actual, expected)
 
 if __name__ == '__main__':
     unittest.main()

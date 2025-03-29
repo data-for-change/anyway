ae8eb1412f98e9b277d9f807967045ab8305927e
ziv <ziv_harpaz@yahoo.com>
2025-03-29 06:59:27 +0300

    Fix CORS for /city

--- anyway/app_and_db.py
+++ anyway/app_and_db.py
@@ -45,10 +45,11 @@ def get_cors_config() -> dict:
         r"/api/streets-by-yishuv": {"origins": "*"},
         r"/api/segments-by-segment": {"origins": "*"},
         r"/api/city": {"origins": "*"},
+        r"/city": {"origins": "*"},
+        r"/involved/*": {"origins": "*"},
         r"/api/comments": {"origins": "*"},
         r"/authorize/*": {"origins": cors_site_list, "supports_credentials": True},
         r"/callback/*": {"origins": cors_site_list, "supports_credentials": True},
-        r"/involved/*": {"origins": cors_site_list, "supports_credentials": True},
         r"/user/*": {"origins": cors_site_list, "supports_credentials": True},
         r"/logout": {"origins": cors_site_list, "supports_credentials": True},
     }

1e9f88a8b4f5c9becb93031fd3fa0e6f0f230fe6
Tamar Kalir <tkalir@gmail.com>
2026-05-18 23:07:55 +0300

    fixing get_file_type_and_year

--- anyway/field_names.py
+++ anyway/field_names.py
@@ -1,6 +1,7 @@
 file_type_police = "SUG_TIK_MISHTARA"
 file_type = "SUG_TIK"
 new_file_type = "SemelSugTikLMS"
+new_accident_year = "ShnatTeuna"
 settlement = "SemelYeshuv"
 street1 = "REHOV1"
 street2 = "REHOV2"
